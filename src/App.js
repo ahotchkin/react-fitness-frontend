e1938e5593cb5bb7aaeb35afb6b426e99c99ba22
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-10 18:15:45 -0400

    Connect to Redux, add componentDidMount method to get the current user when the component mounts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Login from './components/Login'
+import { connect } from 'react-redux'
+import { getCurrentUser } from './actions/currentUser'
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.getCurrentUser()
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,4 +20,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(null, { getCurrentUser })(App);
