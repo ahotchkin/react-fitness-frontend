71833e8d619fe37389a92c8a107efb5299b357e5
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-10 18:17:44 -0400

    Include credentials in fetch requests, complete login fetch request to log a user in, create getCurrentUser function to ensure user is not logged out on page refresh

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -10,15 +10,51 @@ export const setCurrentUser = user => {
 
 // aysnchronous action creators
 export const login = credentials => {
+  console.log("credentials are", credentials)
   return dispatch => {
+    // can abstract fetch requests into an adapter class and do something like - return Adapter.login(args) or Api.login(args)
     return fetch("http://localhost:3001/api/v1/login", {
+      credentials: "include",
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({username: "ally", password: "password"})
+      body: JSON.stringify(credentials)
     })
-      // .then(response => response.json)
-      // .then(json => dispatch({type: }))
+      .then(response => response.json())
+      // .then(user => dispatch({type: "SET_CURRENT_USER"}))
+      .then(user => {
+        if (user.error) {
+          alert(user.error)
+        } else {
+          dispatch(setCurrentUser(user))
+        }
+      })
+      .catch(console.log())
+  }
+}
+
+
+export const getCurrentUser = () => {
+  return dispatch => {
+    // can abstract fetch requests into an adapter class and do something like - return Adapter.login(args) or Api.login(args)
+    console.log("DISPATCHING CURRENT USER")
+    return fetch("http://localhost:3001/api/v1/get_current_user", {
+      credentials: "include",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+      .then(response => response.json())
+      // .then(user => dispatch({type: "SET_CURRENT_USER"}))
+      .then(user => {
+        if (user.error) {
+          alert(user.error)
+        } else {
+          dispatch(setCurrentUser(user))
+        }
+      })
+      .catch(console.log())
   }
 }
