bb51adf14de8c72fce8618ff2dd559f5affde8d5
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-11 14:34:29 -0400

    Add case for CLEAR_CURRENT_USER

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -2,6 +2,9 @@ export default (state = null, action) => {
   switch (action.type) {
     case "SET_CURRENT_USER":
       return action.user
+    case "CLEAR_CURRENT_USER":
+      // want the user to be null when the current user logs out
+      return null
     default:
       return state
   }
