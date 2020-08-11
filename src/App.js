e9f140c1a99cb2ec4d7d6419ec2226fa21879e88
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-11 14:30:46 -0400

    Import Logout, add conditional for Login and Logout components, add mapStateToProps to get currentUser

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Login from './components/Login'
+import Logout from './components/Logout'
 import { connect } from 'react-redux'
 import { getCurrentUser } from './actions/currentUser'
 
 class App extends Component {
 
+  // when app mounts, I want to get my currentUser
   componentDidMount() {
     this.props.getCurrentUser()
   }
@@ -13,11 +15,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        App
-        <Login />
+        {/* move to navBar */}
+        { this.props.currentUser ? <Logout /> : <Login /> }
       </div>
     );
   }
 }
 
-export default connect(null, { getCurrentUser })(App);
+const mapStateToProps = state => {
+  return {
+    currentUser: state.currentUser
+  }
+}
+
+export default connect(mapStateToProps, { getCurrentUser })(App);
