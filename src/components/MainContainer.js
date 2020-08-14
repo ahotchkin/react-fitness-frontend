6b18df36d44b8d30a095644782435ef6f1e59186
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-14 12:07:33 -0400

    Import and render Exercises

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Exercises from './Exercises'
 
 const MainContainer = () => {
   return (
     <div className="MainContainer">
       I am the Main Container!!!!
+      <Exercises />
     </div>
   )
 }
