c027a633f35a96c8016465f216e54aa2b13f55df
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-14 18:12:11 -0400

    Add import signUpForm reducer and add to reducers function

--- src/store.js
+++ src/store.js
@@ -6,12 +6,15 @@ import users from './reducers/users'
 import currentUser from './reducers/currentUser'
 import loginForm from './reducers/loginForm'
 import exercises from './reducers/exercises'
+import signUpForm from './reducers/signUpForm'
 
+// could separate this into a separate rootReducer file
 const reducer = combineReducers({
   users,
   currentUser,
   loginForm,
-  exercises
+  exercises,
+  signUpForm
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
