a76a1c54a5d428b16f25b640f19e09f6ed226cd4
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-16 14:45:31 -0400

    Remove Login and Logout components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import Login from './Login'
-import Logout from './Logout'
 
 const NavBar = ({ currentUser }) => {
 
   return (
     <div className="NavBar">
       { currentUser ? <h2>Welcome, {currentUser.attributes.username}!</h2> : "" }
-      <button>Log In</button>
-      OR
-      <button>Sign Up</button>
-      {/* { currentUser ? <Logout /> : <Login /> } */}
     </div>
   )
 }
