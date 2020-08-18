8cc542d97dab486320ae897fcd3c2f5d306026e7
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-18 17:42:18 -0400

    Comment out all code after rewriting Login.js as React container component

--- src/actions/signUpForm.js
+++ src/actions/signUpForm.js
@@ -1,16 +1,19 @@
+// NOT NEEDED IF LOGIN.JS IS GOING TO BE WRITTE IN REACT INSTEAD OF REDUX
+
+
 // synchronous action creators
-export const updateSignUpForm = formData => {
-  return {
-    type: "UPDATE_SIGNUP_FORM",
-    formData
-    // payload: formData
-
-  }
-}
-
-
-export const resetSignUpForm = () => {
-  return {
-    type: "RESET_SIGNUP_FORM"
-  }
-}
+// export const updateSignUpForm = formData => {
+//   return {
+//     type: "UPDATE_SIGNUP_FORM",
+//     formData
+//     // payload: formData
+//
+//   }
+// }
+//
+//
+// export const resetSignUpForm = () => {
+//   return {
+//     type: "RESET_SIGNUP_FORM"
+//   }
+// }
