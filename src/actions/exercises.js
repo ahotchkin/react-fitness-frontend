be1cfd2c7220d54bdd25ea06fa4802ddb8a034f1
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-18 12:35:33 -0400

    Add addExercise function, send post request to api to add exercise to database, dispatch to add exercise to Redux store

--- src/actions/exercises.js
+++ src/actions/exercises.js
@@ -34,3 +34,38 @@ export const getExercises = () => {
 
   }
 }
+
+export const addExercise = (exerciseData, currentUser, history) => {
+  console.log("exercise data is ", exerciseData)
+  const exercise = {
+    // is there a cleaner way to do this???
+    user_id: currentUser.id,
+    name: exerciseData.name,
+    category: exerciseData.category,
+    duration_in_minutes: exerciseData.duration_in_minutes,
+    calories_burned: exerciseData.calories_burned
+  }
+
+  return dispatch => {
+    return fetch("http://localhost:3001/api/v1/exercises", {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(exercise)
+    })
+      .then(response => response.json())
+      .then(json => {
+        if (json.error) {
+          alert(json.error)
+        } else {
+          console.log(json)
+          dispatch({ type: "ADD_EXERCISE", exercises: json.data })
+          // should they go back to home page or to exercise show page???
+          history.push("/")
+        }
+      })
+      .catch(console.log())
+  }
+}
