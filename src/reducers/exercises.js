cd4685f4beb3c35e42a95fef98fbb00f562ae1c1
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-18 12:38:45 -0400

    Add case for ADD_EXERCISE type

--- src/reducers/exercises.js
+++ src/reducers/exercises.js
@@ -2,6 +2,11 @@ export default (state = [], action) => {
   switch (action.type) {
     case "SET_MY_EXERCISES":
       return action.exercises
+    case "ADD_EXERCISE":
+      return [
+        ...state,
+        action.exercises
+      ]
     default:
       return state
   }
