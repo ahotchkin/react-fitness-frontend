43c51aa570fb67787ea48b5d8f08fa68b20cf1d2
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-19 15:18:58 -0400

    Add case to CLEAR_EXERCISES and set exercises to empty array

--- src/reducers/exercises.js
+++ src/reducers/exercises.js
@@ -1,12 +1,15 @@
 export default (state = [], action) => {
   switch (action.type) {
-    case "SET_MY_EXERCISES":
+    case "SET_EXERCISES":
       return action.exercises
     case "ADD_EXERCISE":
       return [
         ...state,
         action.exercises
       ]
+    case "CLEAR_EXERCISES":
+      // want the exercises to be empty when the current user logs out
+      return []
     default:
       return state
   }
