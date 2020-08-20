9b27935f87d176df762c6da232ee1b9e52a217ee
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-20 17:00:44 -0400

    Import and render Meals

--- src/components/DiaryCard.js
+++ src/components/DiaryCard.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Meals from './Meals';
 
 const DiaryCard = ({ diary }) => {
   return (
     <div>
       <h3>Meal Diary for {diary.attributes.date}</h3>
+      <Meals />
     </div>
 
   )
