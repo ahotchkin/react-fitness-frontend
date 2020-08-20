ab4f6129abdb026a7dfafac5b84f16a6be190bb3
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-20 14:48:54 -0400

    Update name to Diaries, import DiaryCard component and map through all diaries

---
+++ src/components/Diaries.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import DiaryCard from './DiaryCard.js';
+// import Meals from './Meals';
+
+const Diaries = props => {
+  // const diary = props.diaries.first
+  const diaryCards = props.diaries.map(diary => <DiaryCard diary={diary} key={diary.id} />)
+
+
+  // in return, going more than one level deep, so need to make sure exerciseCards are there before rendering anything. instead of just saying exerciseCards, need to say: { exerciseCards.length > 0 ? exerciseCards : null }
+  // <Link to="/exercises/new">
+  //   <button type="button">
+  //     Add Exercise
+  //   </button>
+  // </Link>
+  return (
+    <div>
+      <h2>Diaries</h2>
+      {/* props.diaries is an empty array on page refresh, but is populated on login...... what is happening here*/}
+      { diaryCards.length > 0 ? diaryCards : null }
+
+
+
+    </div>
+  )
+}
+
+// receives the state of the Redux store as an argument
+const mapStateToProps = state => {
+  return {
+    diaries: state.diaries
+  }
+}
+
+// the function returned from invoking connect taht will now supply Exercises with props included state as descriped in MSTP and actions as described in MDTP takes Exercises as an argument - the whole expression is a connected Exercises component with state and actions
+// not just exporting the const from above, but exporting a bulked up version with state and actions
+export default connect(mapStateToProps)(Diaries)
