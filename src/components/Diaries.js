ad6d9993c07736e7ffd0c0c6cf388372242fd546
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-20 17:00:23 -0400

    Add logic to get today's date, only render DiaryCard for diary that matches that date

--- src/components/Diaries.js
+++ src/components/Diaries.js
@@ -5,8 +5,24 @@ import DiaryCard from './DiaryCard.js';
 // import Meals from './Meals';
 
 const Diaries = props => {
-  // const diary = props.diaries.first
-  const diaryCards = props.diaries.map(diary => <DiaryCard diary={diary} key={diary.id} />)
+
+  const today = new Date();
+  const day = today.getDate();
+  const dd = () => { return (day < 10 ? '0' : '') + day }
+  const month = today.getMonth() + 1
+  const mm = () => { return (month < 10 ? '0' : '') + month }
+  const yyyy = today.getFullYear();
+  const date = yyyy+'-'+mm+'-'+dd
+
+
+  const diaryCards = props.diaries.map(diary => {
+    if (diary.attributes.date === `${yyyy}-${mm()}-${dd()}`) {
+      {console.log(diary.attributes.date === `${yyyy}-${mm()}-${dd()}`)}
+
+      return <DiaryCard diary={diary} key={diary.id} />
+    }
+  })
+
 
 
   // in return, going more than one level deep, so need to make sure exerciseCards are there before rendering anything. instead of just saying exerciseCards, need to say: { exerciseCards.length > 0 ? exerciseCards : null }
@@ -19,6 +35,7 @@ const Diaries = props => {
     <div>
       <h2>Diaries</h2>
       {/* props.diaries is an empty array on page refresh, but is populated on login...... what is happening here*/}
+
       { diaryCards.length > 0 ? diaryCards : null }
 
 
