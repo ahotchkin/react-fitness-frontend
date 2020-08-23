8aea10087869af7bf5e3d02543fd7ef95a542eb2
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-23 18:01:26 -0400

    Pass props to access all foods from a certain meal

--- src/components/MealCard.js
+++ src/components/MealCard.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-const MealCard = ({ meal }) => {
+const MealCard = (props) => {
   return (
     <div>
-      <h3>{meal.attributes.category}</h3>
-      <p>Calories: {meal.attributes.calories}</p>
+      <h3>{props.meal.attributes.category}</h3>
+      <p>Calories: {props.meal.attributes.calories}</p>
       {console.log("meal will go here")}
+      {console.log(props.foods)}
+      {console.log(props)}
+      <p>Here's what you ate today</p>
+      <Foods foods={props.foods} />
     </div>
 
   )
