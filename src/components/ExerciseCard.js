59ca4ce127644c9ae413527ee916b29ca22b3e51
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-03 17:09:35 -0400

    Update method name to handleDeleteClick, add commented out function for handleUpdateClick

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -3,10 +3,16 @@ import { connect } from 'react-redux';
 import { deleteExercise } from '../actions/exercises';
 
 
-      // <button onClick={handleClick}>Delete</button>
 const ExerciseCard = (props) => {
 
-  const handleClick = () => {
+  // const handleUpdateClick = () => {
+  //   console.log("updating exercise here")
+  // }
+  // <form onSubmit={handleUpdateClick}>
+  //   <input type="submit" value="Update" />
+  // </form>
+
+  const handleDeleteClick = () => {
     props.deleteExercise(props.exercise.id)
   }
 
@@ -14,7 +20,7 @@ const ExerciseCard = (props) => {
     <div>
       <h3>{props.exercise.attributes.name}</h3>
       <p>Calories Burned: {props.exercise.attributes.calories_burned}</p>
-      <form onSubmit={handleClick}>
+      <form onSubmit={handleDeleteClick}>
         <input type="submit" value="Delete" />
       </form>
 
