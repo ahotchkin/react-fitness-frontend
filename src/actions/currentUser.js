344d3bf194b4f591db54e2fb4853af3d608bcd0d
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-06 17:49:10 -0400

    Comment out references to getExercises

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -1,7 +1,7 @@
 // only needed since LoginForm is in Redux, not react
 // import { resetLoginForm } from './loginForm'
 // import { resetSignUpForm } from './signUpForm'
-import { getExercises } from './exercises';
+// import { getExercises } from './exercises';
 import { clearExercises } from './exercises';
 import { getDiaries } from './diaries';
 import { getMeals } from './meals';
@@ -37,7 +37,7 @@ export const login = (credentials, history) => {
           alert(json.error)
         } else {
           dispatch(setCurrentUser(json.data))
-          dispatch(getExercises())
+          // dispatch(getExercises())
           dispatch(getDiaries())
           dispatch(getMeals())
           // only needed because Login is in Redux and not React
@@ -72,7 +72,7 @@ export const signUp = (credentials, history) => {
           alert(json.error)
         } else {
           dispatch(setCurrentUser(json.data))
-          dispatch(getExercises())
+          // dispatch(getExercises())
           dispatch(getDiaries())
           dispatch(getMeals())
 
@@ -121,7 +121,7 @@ export const getCurrentUser = () => {
           alert(json.error)
         } else {
           dispatch(setCurrentUser(json.data))
-          dispatch(getExercises())
+          // dispatch(getExercises())
           dispatch(getDiaries())
           dispatch(getMeals())
 
