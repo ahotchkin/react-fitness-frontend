45a733a82251adc4bd9c2817229eca2a9678c101
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-08 17:34:48 -0400

    Fix UPDATE_EXERCISE case, begin to fix DELETE_EXERCISE case

--- src/reducers/exercises.js
+++ src/reducers/exercises.js
@@ -8,21 +8,18 @@ export default (state = [], action) => {
         action.exercise
       ]
     case "UPDATE_EXERCISE":
-      return [
-        state.map(exercise => {
-          if (exercise.id === action.exercise.id ) {
-            return action.exercise
-          } else {
-            return exercise
-          }
-        }),
-      ]
-      console.log("THE EXERCISE HAS NOW BEEN UPDATED")
+      return (state.map(exercise => {
+        if (exercise.id === action.exercise.id ) {
+          return action.exercise
+        } else {
+          return exercise
+        }
+      }))
     case "DELETE_EXERCISE":
-      return [
+      console.log(state.filter(exercise => exercise !== action.exercise))
+      return (
         state.filter(exercise => exercise !== action.exercise)
-
-      ]
+      )
     case "CLEAR_EXERCISES":
       // want the exercises to be empty when the current user logs out
       return []
