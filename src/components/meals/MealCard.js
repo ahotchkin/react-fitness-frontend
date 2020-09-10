e40adfcff33b6b4816186bb018962fa0080bfc4f
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-10 16:14:51 -0400

    Add MealCard to meals folder and remove unnecessary console.logs

--- src/components/meals/MealCard.js
+++ src/components/meals/MealCard.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import Foods from './Foods';
+import Foods from '../foods/Foods';
 
 const MealCard = (props) => {
   return (
     <div>
       <h3>{props.meal.attributes.category}</h3>
       <p>Calories: {props.meal.attributes.calories}</p>
-      {console.log("meal will go here")}
-      {console.log(props.foods)}
-      {console.log(props)}
       <p>Here's what you ate today</p>
       <Foods foods={props.foods} />
     </div>
