fc25d5684033965e5bcb938797b474b616c8b100
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-10 16:17:20 -0400

    Temporarily fix path to MealsContainer

--- src/components/diaries/DiaryCard.js
+++ src/components/diaries/DiaryCard.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Meals from '../Meals';
+
+// CAN'T GET THE RELATIVE PATH TO WORK....
+import MealsContainer from '/Users/allysonhotchkin/Development/code/react-fitness-app/react-fitness-frontend/src/containers/MealsContainer';
 
 const DiaryCard = ({ diary }) => {
   return (
     <div>
       <h3>Meal Diary for {diary.attributes.date}</h3>
-      <Meals />
+      <MealsContainer />
     </div>
 
   )
