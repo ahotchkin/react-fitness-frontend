f5a7820bf580097e4fc6ef962c5c6fe71b0381bf
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-10 16:16:15 -0400

    Move Meals to meals folder and remove all connections to Redux store

--- src/components/meals/Meals.js
+++ src/components/meals/Meals.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import MealCard from './MealCard.js';
 
 const Meals = props => {
-  // const diary = props.diaries.first
-  console.log(props.meals)
   const mealCards = props.meals.map(meal => <MealCard meal={meal} key={meal.id} foods={meal.attributes.foods} />)
 
 
@@ -20,20 +16,8 @@ const Meals = props => {
       <h2>Today's Meals</h2>
       {/* props.diaries is an empty array on page refresh, but is populated on login...... what is happening here*/}
       { mealCards.length > 0 ? mealCards : null }
-
-
-
     </div>
   )
 }
 
-// receives the state of the Redux store as an argument
-const mapStateToProps = state => {
-  return {
-    meals: state.meals
-  }
-}
-
-// the function returned from invoking connect taht will now supply Exercises with props included state as descriped in MSTP and actions as described in MDTP takes Exercises as an argument - the whole expression is a connected Exercises component with state and actions
-// not just exporting the const from above, but exporting a bulked up version with state and actions
-export default connect(mapStateToProps)(Meals)
+export default Meals;
