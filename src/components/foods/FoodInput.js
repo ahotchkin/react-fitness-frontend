cb8799ba9c5906e6ac9bc76b540582a4835b3f22
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-11 17:19:00 -0400

    Add form to add new food

---
+++ src/components/foods/FoodInput.js
@@ -0,0 +1,300 @@
+import React, { Component } from 'react';
+
+class FoodInput extends Component {
+
+  state = {
+    brand_name: "",
+    description: "",
+    serving_size: "",
+    servings_per_container: "",
+    calories: "",
+    total_fat: "",
+    saturated_fat: "",
+    polyunsaturated_fat: "",
+    monounsaturated_fat: "",
+    trans_fat: "",
+    cholesterol: "",
+    sodium: "",
+    total_carbohydrate: "",
+    dietary_fiber: "",
+    total_sugars: "",
+    added_sugars: "",
+    sugar_alcohols: "",
+    protein: "",
+    vitamin_a: "",
+    vitamin_c: "",
+    vitamin_d: "",
+    calcium: "",
+    iron: "",
+    potassium: "",
+  }
+
+  handleOnChange = event => {
+    // is this necessary?
+    event.persist()
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    // set the state here by accessing props provided by mapDispatchToProps
+    console.log(this.props)
+    this.props.createExercise(this.state, this.props.history)
+    this.setState({
+      brand_name: "",
+      description: "",
+      serving_size: "",
+      servings_per_container: "",
+      calories: "",
+      total_fat: "",
+      saturated_fat: "",
+      polyunsaturated_fat: "",
+      monounsaturated_fat: "",
+      trans_fat: "",
+      cholesterol: "",
+      sodium: "",
+      total_carbohydrate: "",
+      dietary_fiber: "",
+      total_sugars: "",
+      added_sugars: "",
+      sugar_alcohols: "",
+      protein: "",
+      vitamin_a: "",
+      vitamin_c: "",
+      vitamin_d: "",
+      calcium: "",
+      iron: "",
+      potassium: "",
+    })
+    console.log("you submitted your food!!!")
+  };
+
+  render() {
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <label>Brand Name: </label>
+        <input
+          name="brand_name"
+          onChange={this.handleOnChange}
+          value={this.state.brand_name}
+        />
+
+        <br />
+
+        <label>Description: </label>
+        <input
+          name="description"
+          onChange={this.handleOnChange}
+          value={this.state.description}
+        />
+
+        <br />
+
+        <label>Serving Size: </label>
+        <input
+          name="serving_size"
+          onChange={this.handleOnChange}
+          value={this.state.serving_size}
+        />
+
+        <br />
+
+        <label>Servings Per Container: </label>
+        <input
+          name="servings_per_container"
+          onChange={this.handleOnChange}
+          value={this.state.servings_per_container}
+        />
+
+        <br />
+
+        <label>Calories: </label>
+        <input
+          name="calories"
+          onChange={this.handleOnChange}
+          value={this.state.calories}
+        />
+
+        <br />
+
+        <label>Total Fat (g): </label>
+        <input
+          name="total_fat"
+          onChange={this.handleOnChange}
+          value={this.state.total_fat}
+        />
+
+        <br />
+
+        <label>Saturated Fat (g): </label>
+        <input
+          name="saturated_fat"
+          onChange={this.handleOnChange}
+          value={this.state.saturated_fat}
+        />
+
+        <br />
+
+        <label>Polyunsaturated Fat (g): </label>
+        <input
+          name="polyunsaturated_fat"
+          onChange={this.handleOnChange}
+          value={this.state.polyunsaturated_fat}
+        />
+
+        <br />
+
+        <label>Monounsaturated Fat (g): </label>
+        <input
+          name="monounsaturated_fat"
+          onChange={this.handleOnChange}
+          value={this.state.monounsaturated_fat}
+        />
+
+        <br />
+
+        <label>Trans Fat (g): </label>
+        <input
+          name="trans_fat"
+          onChange={this.handleOnChange}
+          value={this.state.trans_fat}
+        />
+
+        <br />
+
+        <label>Cholesterol (mg): </label>
+        <input
+          name="cholesterol"
+          onChange={this.handleOnChange}
+          value={this.state.cholesterol}
+        />
+
+        <br />
+
+        <label>Sodium (mg): </label>
+        <input
+          name="sodium"
+          onChange={this.handleOnChange}
+          value={this.state.sodium}
+        />
+
+        <br />
+
+        <label>Total Carbohydrates (g): </label>
+        <input
+          name="total_carbohydrate"
+          onChange={this.handleOnChange}
+          value={this.state.total_carbohydrate}
+        />
+
+        <br />
+
+        <label>Dietary Fiber (g): </label>
+        <input
+          name="dietary_fiber"
+          onChange={this.handleOnChange}
+          value={this.state.dietary_fiber}
+        />
+
+        <br />
+
+        <label>Total Sugars (g): </label>
+        <input
+          name="total_sugars"
+          onChange={this.handleOnChange}
+          value={this.state.total_sugars}
+        />
+
+        <br />
+
+        <label>Added Sugars (g): </label>
+        <input
+          name="added_sugars"
+          onChange={this.handleOnChange}
+          value={this.state.added_sugars}
+        />
+
+        <br />
+
+        <label>Sugar Alcohols (g): </label>
+        <input
+          name="sugar_alcohols"
+          onChange={this.handleOnChange}
+          value={this.state.sugar_alcohols}
+        />
+
+        <br />
+
+        <label>Protein (g): </label>
+        <input
+          name="protein"
+          onChange={this.handleOnChange}
+          value={this.state.protein}
+        />
+
+        <br />
+
+        <label>Vitamin A (%): </label>
+        <input
+          name="vitamin_a"
+          onChange={this.handleOnChange}
+          value={this.state.vitamin_a}
+        />
+
+        <br />
+
+        <label>Vitamin C (%): </label>
+        <input
+          name="vitamin_c"
+          onChange={this.handleOnChange}
+          value={this.state.vitamin_c}
+        />
+
+        <br />
+
+        <label>Vitamin D (%): </label>
+        <input
+          name="vitamin_d"
+          onChange={this.handleOnChange}
+          value={this.state.vitamin_d}
+        />
+
+        <br />
+
+        <label>Calcium (%): </label>
+        <input
+          name="calcium"
+          onChange={this.handleOnChange}
+          value={this.state.calcium}
+        />
+
+        <br />
+
+        <label>Iron (%): </label>
+        <input
+          name="iron"
+          onChange={this.handleOnChange}
+          value={this.state.iron}
+        />
+
+        <br />
+
+        <label>Potassium (mg): </label>
+        <input
+          name="potassium"
+          onChange={this.handleOnChange}
+          value={this.state.potassium}
+        />
+
+        <br />
+
+        <input type="submit" value="Add Exercise" />
+      </form>
+
+    )
+  }
+}
+
+export default FoodInput;
