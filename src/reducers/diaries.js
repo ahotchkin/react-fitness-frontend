9c4eb4fdc6e997f417bb3097bf75a4c1de400d2e
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-11 15:14:05 -0400

    Add case for ADD_DIARY

--- src/reducers/diaries.js
+++ src/reducers/diaries.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
   switch (action.type) {
     case "SET_DIARIES":
       return action.diaries
+    case "ADD_DIARY":
+      return state.concat(action.diary)
     case "CLEAR_DIARIES":
       // want the diaries to be empty when the current user logs out
       return []
