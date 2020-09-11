dc3b714182545e5de7d6095214190cc2113992f5
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-11 15:11:05 -0400

    Add actions to add and create diaries

--- src/actions/diaries.js
+++ src/actions/diaries.js
@@ -13,6 +13,14 @@ export const clearDiaries = () => {
   }
 }
 
+export const addDiary = diary => {
+  return {
+    type: "ADD_DIARY",
+    diary
+  }
+}
+
+
 // before you think about populating this piece of state with anything, get it into the store first to see the name and data type are correct
 // steps: 1. Build reducer, 2. Add to store, 3. Build action creator
 
@@ -41,37 +49,35 @@ export const getDiaries = () => {
   }
 }
 
-// export const addExercise = (exerciseData, currentUser, history) => {
-//   console.log("exercise data is ", exerciseData)
-//   const exercise = {
-//     // is there a cleaner way to do this???
-//     user_id: currentUser.id,
-//     name: exerciseData.name,
-//     category: exerciseData.category,
-//     duration_in_minutes: exerciseData.duration_in_minutes,
-//     calories_burned: exerciseData.calories_burned
-//   }
-//
-//   return dispatch => {
-//     return fetch("http://localhost:3001/api/v1/exercises", {
-//       credentials: "include",
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json"
-//       },
-//       body: JSON.stringify(exercise)
-//     })
-//       .then(response => response.json())
-//       .then(json => {
-//         if (json.error) {
-//           alert(json.error)
-//         } else {
-//           console.log(json)
-//           dispatch({ type: "ADD_EXERCISE", exercises: json.data })
-//           // should they go back to home page or to exercise show page???
-//           history.push("/")
-//         }
-//       })
-//       .catch(console.log())
-//   }
-// }
+export const createDiary = (todaysDate, currentUser, history) => {
+  console.log("Diary date is", todaysDate)
+  const diary = {
+    // is there a cleaner way to do this???
+    user_id: currentUser.id,
+    date: todaysDate,
+  }
+
+  return dispatch => {
+    return fetch("http://localhost:3001/api/v1/diaries", {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(diary)
+    })
+      .then(response => response.json())
+      .then(json => {
+        if (json.error) {
+          alert(json.error)
+        } else {
+          console.log(json)
+          dispatch(addDiary(json.data))
+          console.log("diary successfully created")
+          // should they go back to home page or to diary show page???
+          history.push("/diaries")
+        }
+      })
+      .catch(console.log())
+  }
+}
