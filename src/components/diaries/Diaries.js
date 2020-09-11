710d2d8dab7180aceb1f9f1af50607976f4a8ebb
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-11 15:13:07 -0400

    Add button and functionality to create today's diary

--- src/components/diaries/Diaries.js
+++ src/components/diaries/Diaries.js
@@ -3,24 +3,25 @@ import DiaryCard from './DiaryCard';
 
 const Diaries = props => {
 
+
   const today = new Date();
   const day = today.getDate();
   const dd = () => { return (day < 10 ? '0' : '') + day }
   const month = today.getMonth() + 1
   const mm = () => { return (month < 10 ? '0' : '') + month }
   const yyyy = today.getFullYear();
-
+  const todaysDate = `${yyyy}-${mm()}-${dd()}`
 
   // filter desired diary before mapping to avoid a situation where nothing would be returned at the end of map - necessary to avoid warning: Expected to return a value at the end of arrow function array-callback-return
-  const diaryCard = props.diaries.filter(diary => diary.attributes.date === `${yyyy}-${mm()}-${dd()}`).map(filteredDiary => <DiaryCard diary={filteredDiary} key={filteredDiary.id} />)
+  const diaryCard = props.diaries.filter(diary => diary.attributes.date === todaysDate).map(filteredDiary => <DiaryCard diary={filteredDiary} key={filteredDiary.id} />)
 
   return (
     <div>
       <h2>Diaries</h2>
       {/* props.diaries is an empty array on page refresh, but is populated on login...... what is happening here*/}
-
-      {/* is this necessary? check on day when no diary exists */}
-      { diaryCard.length > 0 ? diaryCard : null }
+      {console.log(props)}
+      {/* move createDiary to separate function? */}
+      { diaryCard.length > 0 ? diaryCard : <button onClick={() => props.createDiary(todaysDate, props.currentUser, props.history)}>Start Today's Meal Diary</button> }
     </div>
   )
 }
