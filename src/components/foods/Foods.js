1ca3f147cfcc8720d7f792e6a0f77ef844fa4d04
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-14 16:52:13 -0400

    Pass mealId as a prop to FoodCard, add search field

--- src/components/foods/Foods.js
+++ src/components/foods/Foods.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import FoodCard from './FoodCard.js';
+import FoodCard from './FoodCard';
+import SearchInput from '../SearchInput'
 
 const Foods = (props) => {
 
-  console.log(props)
   console.log("meal id is", props.location.state.mealId)
 
-  // const foodCards = props.foods.map(food => <FoodCard key={food.id} food={food} />)
+  const foodCards = props.foods.map(food => <FoodCard key={food.id} food={food} mealId={props.location.state.mealId} />)
 
-
-  // in return, going more than one level deep, so need to make sure exerciseCards are there before rendering anything. instead of just saying exerciseCards, need to say: { exerciseCards.length > 0 ? exerciseCards : null }
-  // <Link to="/exercises/new">
-  //   <button type="button">
-  //     Add Exercise
-  //   </button>
-  // </Link>
   return (
     <div>
       <h2>Foods</h2>
       {/* props.foods is an empty array on page refresh, but is populated on login...... what is happening here*/}
 
-      {/* { foodCards.length > 0 ? foodCards : null } */}
+      { foodCards.length > 0 ? foodCards : null }
 
       <p>Search the database for a food or add a new food</p>
+
+      <SearchInput />
+
       <Link to="/foods/new">
         <button type="button">
           Add New Food
@@ -34,8 +30,4 @@ const Foods = (props) => {
   )
 }
 
-
-// the function returned from invoking connect taht will now supply Exercises with props included state as descriped in MSTP and actions as described in MDTP takes Exercises as an argument - the whole expression is a connected Exercises component with state and actions
-// not just exporting the const from above, but exporting a bulked up version with state and actions
-// export default connect(mapStateToProps)(Foods)
-export default Foods
+export default Foods;
