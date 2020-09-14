a0968aef3c0eebc5bfc21f9669854f50922cff72
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-14 14:50:03 -0400

    Add link to Add Food and pass mealId as state to have access

--- src/components/meals/MealCard.js
+++ src/components/meals/MealCard.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import MealFoods from '../mealFoods/MealFoods';
 
 const MealCard = (props) => {
@@ -8,6 +10,18 @@ const MealCard = (props) => {
       <h3>{props.meal.attributes.category}</h3>
       <p>Calories: {props.meal.attributes.calories}</p>
       <MealFoods mealFoods={props.mealFoods} />
+
+      {/* is there a better way to provide the meal id so the food added can be associated? */}
+      <Link to={{
+        pathname: "/foods",
+        state: {
+          mealId: props.meal.id
+        }
+      }}>
+        <button type="button">
+          Add Food
+        </button>
+      </Link>
     </div>
 
   )
