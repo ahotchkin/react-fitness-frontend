5743a0e2937cb0dd6e54fca69d227e187f209aae
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-14 14:49:26 -0400

    Remove links and update references of foods to mealFoods

--- src/components/mealFoods/MealFoods.js
+++ src/components/mealFoods/MealFoods.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import MealFoodCard from './MealFoodCard.js';
 
 const MealFoods = (props) => {
 
   console.log(props)
-  const mealFoodCards = props.mealFoods.map(food => <MealFoodCard key={food.id} mealFood={food} />)
+  const mealFoodCards = props.mealFoods.map(mealFood => <MealFoodCard key={mealFood.id} mealFood={mealFood} />)
 
 
   // in return, going more than one level deep, so need to make sure exerciseCards are there before rendering anything. instead of just saying exerciseCards, need to say: { exerciseCards.length > 0 ? exerciseCards : null }
@@ -20,11 +19,7 @@ const MealFoods = (props) => {
       {/* props.foods is an empty array on page refresh, but is populated on login...... what is happening here*/}
 
       { mealFoodCards.length > 0 ? mealFoodCards : null }
-      <Link to="/foods">
-        <button type="button">
-          Add Food
-        </button>
-      </Link>
+
 
 
     </div>
