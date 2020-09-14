d446228a7984da57f448726ba888d7faf9211bb2
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-14 16:51:02 -0400

    Add button to add food to meal

--- src/components/foods/FoodCard.js
+++ src/components/foods/FoodCard.js
@@ -5,7 +5,9 @@ const FoodCard = ({ food }) => {
   // right now I'm getting the individual foods from the meals in the database. I have a foods serializer set up so when I want to be able to search through my entire list of foods I can. I shouldn't need my foods action or reducer right now since this information is all coming from meals....
   // ************************************************************************************************
 
-
+  const handleSubmit = () => {
+    
+  }
 
 
 
@@ -13,7 +15,8 @@ const FoodCard = ({ food }) => {
 
   return (
     <div>
-     <h6>{food.brand_name} {food.description} - {food.calories} calories</h6>
+     <p>{food.attributes.brand_name} {food.attributes.description} - {food.attributes.calories} calories</p>
+     <button>Add</button>
     </div>
 
   )
