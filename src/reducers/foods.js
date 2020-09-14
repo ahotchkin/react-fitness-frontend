260dc9ce3b0ec811799c5ac7e797b8946a192420
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-14 16:55:05 -0400

    Update return for GET_FOODS and comment out CLEAR_FOODS

--- src/reducers/foods.js
+++ src/reducers/foods.js
@@ -1,10 +1,10 @@
 export default (state = [], action) => {
   switch (action.type) {
     case "SET_FOODS":
-      return action.diaries
-    case "CLEAR_FOODS":
-      // want the diaries to be empty when the current user logs out
-      return []
+      return action.foods
+    // case "CLEAR_FOODS":
+    //   // want the diaries to be empty when the current user logs out
+    //   return []
     default:
       return state
   }
