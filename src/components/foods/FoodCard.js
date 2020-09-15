c3f4738db9ee8c997cf7e0d18b0b806d40ab69bd
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-14 21:23:36 -0400

    Add handleOnClick to add mealFood

--- src/components/foods/FoodCard.js
+++ src/components/foods/FoodCard.js
@@ -1,22 +1,28 @@
 import React from 'react';
 
-const FoodCard = ({ food }) => {
+const FoodCard = props => {
 
   // right now I'm getting the individual foods from the meals in the database. I have a foods serializer set up so when I want to be able to search through my entire list of foods I can. I shouldn't need my foods action or reducer right now since this information is all coming from meals....
   // ************************************************************************************************
 
-  const handleSubmit = () => {
-    
+  console.log(props)
+
+  const handleOnClick = () => {
+    props.createMealFood(props.mealId, props.food, props.history)
   }
 
+
+  // const handleDeleteClick = () => {
+  //   props.deleteExercise(props.exercise.id, props.history)
+  // }
 
 
 
 
   return (
     <div>
-     <p>{food.attributes.brand_name} {food.attributes.description} - {food.attributes.calories} calories</p>
-     <button>Add</button>
+     <p>{props.food.attributes.brand_name} {props.food.attributes.description} - {props.food.attributes.calories} calories</p>
+     <button onClick={handleOnClick}>Add</button>
     </div>
 
   )
