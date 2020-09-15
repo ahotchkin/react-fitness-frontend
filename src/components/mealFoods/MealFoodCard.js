c318210b6863a7858b60410061bac1412dd2567b
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-14 21:26:15 -0400

    Rename prop to food

--- src/components/mealFoods/MealFoodCard.js
+++ src/components/mealFoods/MealFoodCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const MealFoodCard = ({ mealFood }) => {
+const MealFoodCard = ({ food }) => {
 
   // right now I'm getting the individual foods from the meals in the database. I have a foods serializer set up so when I want to be able to search through my entire list of foods I can. I shouldn't need my foods action or reducer right now since this information is all coming from meals....
   // ************************************************************************************************
@@ -13,8 +13,8 @@ const MealFoodCard = ({ mealFood }) => {
 
   return (
     <div>
-      {console.log(mealFood)}
-      <h6>{mealFood.brand_name} {mealFood.description} - {mealFood.calories} calories</h6>
+      {console.log(food)}
+      <h6>{food.brand_name} {food.description} - {food.calories} calories</h6>
     </div>
 
   )
