f43a85d542f32b5d257e3a8ed8e88278d5822f6e
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-14 21:29:51 -0400

    Update reference of food to mealFood

--- src/reducers/mealFoods.js
+++ src/reducers/mealFoods.js
@@ -1,12 +1,12 @@
 export default (state = [], action) => {
   switch (action.type) {
-    // case "SET_FOODS":
-    //   return action.foods
+    // case "SET_MEAL_FOODS":
+    //   return action.mealFoods
     // case "CLEAR_FOODS":
     //   // want the diaries to be empty when the current user logs out
     //   return []
     case "ADD_MEAL_FOOD":
-      return state.concat(action.food)
+      return state.concat(action.mealFood)
     default:
       return state
   }
