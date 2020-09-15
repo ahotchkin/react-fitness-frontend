9ccca48839864ce3cff052a0cd0eea5261364582
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-15 16:51:02 -0400

    Update to class component, add text field for number of servings to pass to createMealFood

--- src/components/foods/FoodCard.js
+++ src/components/foods/FoodCard.js
@@ -1,31 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const FoodCard = props => {
+class FoodCard extends Component {
 
-  // right now I'm getting the individual foods from the meals in the database. I have a foods serializer set up so when I want to be able to search through my entire list of foods I can. I shouldn't need my foods action or reducer right now since this information is all coming from meals....
-  // ************************************************************************************************
-
-  console.log(props)
-
-  const handleOnClick = () => {
-    props.createMealFood(props.mealId, props.food, props.history)
+  state = {
+    number_of_servings: ""
   }
 
+  handleOnChange = event => {
+    // is this necessary?
+    event.persist()
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
 
-  // const handleDeleteClick = () => {
-  //   props.deleteExercise(props.exercise.id, props.history)
-  // }
+  handleSubmit = event => {
+    event.preventDefault();
+    // set the state here by accessing props provided by mapDispatchToProps
+    console.log(this.props)
+    this.props.createMealFood(this.props.mealId, this.props.food, this.state.number_of_servings, this.props.history)
+    this.setState({
+      text: "",
+    })
+    console.log("adding food to meal.......")
+  };
 
 
 
+  render() {
+    return (
+      <div>
+       <p>{this.props.food.attributes.brand_name} {this.props.food.attributes.description} - {this.props.food.attributes.calories} calories</p>
 
-  return (
-    <div>
-     <p>{props.food.attributes.brand_name} {props.food.attributes.description} - {props.food.attributes.calories} calories</p>
-     <button onClick={handleOnClick}>Add</button>
-    </div>
+       <form onSubmit={this.handleSubmit}>
+         <label>Number of Servings: </label>
+         <input
+           name="number_of_servings"
+           onChange={this.handleOnChange}
+           value={this.state.number_of_servings}
+         />
 
-  )
+         <br />
+
+         <input type="submit" value="Add" />
+
+       </form>
+
+      </div>
+
+    )
+  }
+
 }
 
-export default FoodCard
+export default FoodCard;
