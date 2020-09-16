ceafefaf7dad17fdd2736eb90c9eefb83f0ea196
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-15 20:30:34 -0400

    Create mealCalories const to display total meal calories

--- src/components/meals/MealCard.js
+++ src/components/meals/MealCard.js
@@ -4,11 +4,15 @@ import { Link } from 'react-router-dom';
 import MealFoods from '../mealFoods/MealFoods';
 
 const MealCard = (props) => {
+
+  const mealCalories = props.mealFoods.reduce((a, b) => ({calories: a.calories + b.calories}))
+
+
   return (
     <div>
     {console.log(props)}
       <h3>{props.meal.attributes.category}</h3>
-      <p>Calories: {props.meal.attributes.calories}</p>
+      <p>Calories: {mealCalories.calories}</p>
       <MealFoods mealFoods={props.mealFoods} foods={props.foods} />
 
       {/* is there a better way to provide the meal id so the food added can be associated? */}
