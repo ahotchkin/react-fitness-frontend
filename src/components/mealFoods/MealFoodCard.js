f3730e190851bd4e8393b8460b2ce8d62d9c8102
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-16 20:34:08 -0400

    Add link to update mealFood, remove console.logs

--- src/components/mealFoods/MealFoodCard.js
+++ src/components/mealFoods/MealFoodCard.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+
 
 const MealFoodCard = props => {
 
+  // copied from ExerciseCard.js
+  const handleDeleteClick = () => {
+    props.deleteExercise(props.exercise.id, props.history)
+  }
+
   return (
     <div>
-      {console.log(props.food)}
-      {console.log(props.mealFood)}
-      <h6>{props.food.brand_name} {props.food.description}, {props.mealFood.number_of_servings} servings - {props.mealFood.calories} calories</h6>
+      <p>{props.food.brand_name} {props.food.description}, {props.mealFood.number_of_servings} servings - {props.mealFood.calories} calories -
+      <Link to={`/meal_foods/${props.mealFood.id}/edit`}>
+        <button type="button">
+          Update
+        </button>
+      </Link>
+      <button onClick={handleDeleteClick}>Delete</button>
+      </p>
     </div>
 
   )
