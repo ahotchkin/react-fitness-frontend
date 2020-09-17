a7a97e7dc730adca26204606f2f50823e74b2976
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-17 15:27:40 -0400

    Remove mealFoods as prop sent ot MealCard

--- src/components/meals/Meals.js
+++ src/components/meals/Meals.js
@@ -2,7 +2,8 @@ import React from 'react';
 import MealCard from './MealCard.js';
 
 const Meals = props => {
-  const mealCards = props.meals.map(meal => <MealCard meal={meal} key={meal.id} mealFoods={meal.attributes.meal_foods} foods={meal.attributes.foods} />)
+  // const mealCards = props.meals.map(meal => <MealCard meal={meal} key={meal.id} mealFoods={meal.attributes.meal_foods} foods={meal.attributes.foods} />)
+  const mealCards = props.meals.map(meal => <MealCard meal={meal} key={meal.id} foods={meal.attributes.foods} />)
 
   // in return, going more than one level deep, so need to make sure exerciseCards are there before rendering anything. instead of just saying exerciseCards, need to say: { exerciseCards.length > 0 ? exerciseCards : null }
   // <Link to="/exercises/new">
