8529421f93b73698ec04ded6b981b8a664e3d464
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-17 15:24:51 -0400

    Update references to props since information is now coming from Meal Food

--- src/components/mealFoods/MealFoodCard.js
+++ src/components/mealFoods/MealFoodCard.js
@@ -9,9 +9,13 @@ const MealFoodCard = props => {
     props.deleteExercise(props.exercise.id, props.history)
   }
 
+  // previous <p>:       <p>{props.food.brand_name} {props.food.description}, {props.mealFood.number_of_servings} servings - {props.mealFood.calories} calories - ...
+
+  
   return (
     <div>
-      <p>{props.food.brand_name} {props.food.description}, {props.mealFood.number_of_servings} servings - {props.mealFood.calories} calories -
+    {console.log(props)}
+      <p>{props.mealFood.attributes.food.brand_name} {props.mealFood.attributes.food.description}, {props.mealFood.attributes.number_of_servings} servings - {props.mealFood.attributes.calories} calories -
       <Link to={`/meal_foods/${props.mealFood.id}/edit`}>
         <button type="button">
           Update
