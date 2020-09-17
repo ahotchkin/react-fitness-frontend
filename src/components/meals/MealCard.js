0c6fe0ec45d345c30bf5a70884629d521b5193b3
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-17 15:26:39 -0400

    Render MealFoodsContainer

--- src/components/meals/MealCard.js
+++ src/components/meals/MealCard.js
@@ -3,23 +3,31 @@ import { Link } from 'react-router-dom';
 
 import MealFoods from '../mealFoods/MealFoods';
 
+// *******************************************************************************************************
+import MealFoodsContainer from '/Users/allysonhotchkin/Development/code/react-fitness-app/react-fitness-frontend/src/containers/MealFoodsContainer'
+// *******************************************************************************************************
+
 const MealCard = (props) => {
 
-  let mealCalories = {
-    calories: 0
-  }
-  // // MAKE SURE I UNDERSTAND HOW REDUCE IS WORKING
-  if (props.mealFoods.length > 0) {
-    mealCalories = props.mealFoods.reduce((a, b) => ({calories: a.calories + b.calories}))
-  }
+  // let mealCalories = {
+  //   calories: 0
+  // }
+  // // // MAKE SURE I UNDERSTAND HOW REDUCE IS WORKING
+  // // update this to be handled on the backend and display meal.calories
+  // if (props.mealFoods.length > 0) {
+  //   mealCalories = props.mealFoods.reduce((a, b) => ({calories: a.calories + b.calories}))
+  // }
 
 
   return (
     <div>
-    {console.log(props)}
       <h3>{props.meal.attributes.category}</h3>
-      <p>Calories: {mealCalories.calories}</p>
-      <MealFoods mealFoods={props.mealFoods} foods={props.foods} />
+      {/* <p>Calories: {mealCalories.calories}</p> */}
+      {/*<MealFoods mealFoods={props.mealFoods} foods={props.foods} /> */}
+
+      {/* ******************************************************************************** */}
+      <MealFoodsContainer meal={props.meal} />
+      {/* ******************************************************************************** */}
 
       {/* is there a better way to provide the meal id so the food added can be associated? */}
       <Link to={{
