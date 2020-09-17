5388e7e513585930642fcea1caafddd9a1f2b262
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-17 17:47:14 -0400

    Add case for UPDATE_MEA L

--- src/reducers/meals.js
+++ src/reducers/meals.js
@@ -2,6 +2,14 @@ export default (state = [], action) => {
   switch (action.type) {
     case "SET_MEALS":
       return action.meals
+    case "UPDATE_MEAL":
+      return (state.map(meal => {
+        if (meal.id === action.meal.id ) {
+          return action.meal
+        } else {
+          return meal
+        }
+      }))
     case "CLEAR_MEALS":
       // want the exercises to be empty when the current user logs out
       return []
