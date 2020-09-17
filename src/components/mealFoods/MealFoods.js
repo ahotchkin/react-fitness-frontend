cdbd027045c732750aa6887d9592f498d6541619
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-17 15:26:04 -0400

    Update references to props since information is now coming from MealFood

--- src/components/mealFoods/MealFoods.js
+++ src/components/mealFoods/MealFoods.js
@@ -6,18 +6,24 @@ const MealFoods = (props) => {
   // Need to use mealFood.id as the key since it unique
   // Need to get all other information from food
   // zippedMealFoods combines the two arrays (mealFoods and foods) that are passed as props so all information can be accessed for the MealFoodCard
-  const zippedMealFoods = props.mealFoods.map((mealFood, i) => [mealFood, props.foods[i]])
+  // const zippedMealFoods = props.mealFoods.map((mealFood, i) => [mealFood, props.foods[i]])
+  //
+  // const mealFoodCards = zippedMealFoods.map(zippedMealFood => <MealFoodCard key={zippedMealFood[0].id} mealFood={zippedMealFood[0]} food={zippedMealFood[1]}/>)
 
-  const mealFoodCards = zippedMealFoods.map(zippedMealFood => <MealFoodCard key={zippedMealFood[0].id} mealFood={zippedMealFood[0]} food={zippedMealFood[1]}/>)
 
-  console.log(mealFoodCards)
+
+
+
+  // ****************************************************************************************************************************************************
+  const mealFoodCards = props.mealFoods.map(mealFood => <MealFoodCard key={mealFood.id} mealFood={mealFood} />)
+  // ****************************************************************************************************************************************************
+
   return (
     <div>
-      <h2>Foods for this meal</h2>
+      <h4>Foods for this meal:</h4>
       {/* props.foods is an empty array on page refresh, but is populated on login...... what is happening here*/}
 
       { mealFoodCards.length > 0 ? mealFoodCards : null }
-
 
 
     </div>
