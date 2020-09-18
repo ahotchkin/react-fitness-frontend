61848333ee91779a27ecb7e0e95be29caf91b9eb
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-18 13:39:35 -0400

    Update arguments passed to updateMealFood

--- src/components/mealFoods/MealFoodUpdate.js
+++ src/components/mealFoods/MealFoodUpdate.js
@@ -18,15 +18,12 @@ class MealFoodUpdate extends Component {
   handleSubmit = event => {
     event.preventDefault();
     // set the state here by accessing props provided by mapDispatchToProps
-    console.log(this.props)
-
     // IS IT NOT NECESSARY TO PASS IN THE CURRENT USER BECAUSE ONLY THE FIELDS DECLARED IN THE ACTION WILL BE UPDATED?????????????
-    this.props.updateMealFood(this.props.mealFood, this.props.mealFood.attributes.food, this.state.number_of_servings, this.props.history)
+    this.props.updateMealFood(this.props.mealFood, this.state.number_of_servings, this.props.history)
     this.setState({
       // drop down stays selected on whatever category was selected. Is this a problem or will it always update on page refresh?
       number_of_servings: "",
     })
-    console.log("you updated your mealFood!!!")
   };
 
 
@@ -35,8 +32,6 @@ class MealFoodUpdate extends Component {
   render() {
     return(
       <div>
-      {console.log(this.props)}
-
       <h1>Let's get ready to update!!!</h1>
 
         <p>This is the MEALFOOD UPDATE page for mealFood with an ID of {this.props.mealFood.id}.</p>
