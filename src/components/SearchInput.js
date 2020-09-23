37f22b9300ffde2849bb79854a5d1bf304794a41
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-23 15:37:10 -0400

    Update SearchInput to functional component, remove state, remove handleOnChange and handleSubmit functions

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,46 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class SearchInput extends Component {
+const SearchInput = props => {
 
-  state = {
-    text: "",
-  }
+  return(
+    <form onSubmit={props.handleSubmit}>
+      <input
+        type="text"
+        name="searchTerm"
+        onChange={props.handleOnChange}
+        value={props.searchTerm}
+      />
 
-  handleOnChange = event => {
-    // is this necessary?
-    event.persist()
-    this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
+      <br />
 
-  handleSubmit = event => {
-    event.preventDefault();
-    // set the state here by accessing props provided by mapDispatchToProps
-    console.log(this.props)
-    // this.props.createExercise(this.state, this.props.history)
-    this.setState({
-      text: "",
-    })
-    console.log("searching.......")
-  };
+      <input type="submit" value="Search" />
+    </form>
+  )
 
-  render() {
-    return(
-      <form onSubmit={this.handleSubmit}>
-        <input
-          name="text"
-          onChange={this.handleOnChange}
-          value={this.state.text}
-        />
-
-        <br />
-
-        <input type="submit" value="Search" />
-      </form>
-
-    )
-  }
 }
 
 export default SearchInput;
