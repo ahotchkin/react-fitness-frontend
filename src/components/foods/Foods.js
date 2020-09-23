9c9da77b38d56d867daccef033055e0070ef6dff
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-23 15:38:10 -0400

    Update to class component, add state and search functionality, pass necessary functions and state to SearchInput as props

--- src/components/foods/Foods.js
+++ src/components/foods/Foods.js
@@ -1,44 +1,96 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import FoodCard from './FoodCard';
 import SearchInput from '../SearchInput'
 
-const Foods = props => {
+
+class Foods extends Component {
+
+  state = {
+    searchTerm: "",
+    currentlyDisplayed: this.props.foods,
+  }
+
+  renderFoodCards = () => {
+    // return this.props.foods.map(food => <FoodCard key={food.id} food={food} meal={this.props.meal} createMealFood={this.props.createMealFood} history={this.props.history}/>)
+    return this.state.currentlyDisplayed.map(food => {
+      return (<FoodCard key={food.id} food={food} meal={this.props.meal} createMealFood={this.props.createMealFood} history={this.props.history}/>)
+    })
+  }
+
+  handleOnChange = event => {
+    // is this necessary?
+    event.persist()
+
+    let newlyDisplayed = this.props.foods.filter(food => food.attributes.brand_name.toLowerCase().includes(event.target.value.toLowerCase()) || food.attributes.description.toLowerCase().includes(event.target.value.toLowerCase()))
+
+    this.setState({
+      [event.target.name]: event.target.value,
+      currentlyDisplayed: newlyDisplayed
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    //
+    // this.setState({
+    //   text: "",
+    // })
+    // console.log("searching.......")
+  };
 
 
-  const foodCards = props.foods.map(food => <FoodCard key={food.id} food={food} meal={props.meal} createMealFood={props.createMealFood} history={props.history}/>)
+  render() {
+    return (
+      <div>
+        <h2>Foods</h2>
+        {/* props.foods is an empty array on page refresh, but is populated on login...... what is happening here*/}
+        {console.log(this.props)}
+        {console.log(this.state)}
 
-  return (
-    <div>
-      <h2>Foods</h2>
-      {/* props.foods is an empty array on page refresh, but is populated on login...... what is happening here*/}
+        <p>Search the database for a food or add a new food</p>
 
-      { foodCards.length > 0 ? foodCards : null }
+        <SearchInput searchTerm={this.state.searchTerm} handleOnChange={this.handleOnChange} handleSubmit={this.handleSubmit} />
+        {/*
+        <form onSubmit={this.handleSubmit}>
+          <input
+            name="searchTerm"
+            onChange={this.handleOnChange}
+            value={this.state.text}
+          />
 
-      <p>Search the database for a food or add a new food</p>
+          <br />
 
-      <SearchInput />
+          <input type="submit" value="Search" />
+        </form>
+        */}
 
-      {!!props.meal ?
-        <Link to={{
-          pathname: "/foods/new",
-          state: {
-            mealId: props.meal.id
-          }
-        }}>
-          <button type="button">
-            Add New Food
-          </button>
-        </Link>
-        :
-        <Link to="/foods/new">
-          <button type="button">
-            Add New Food
-          </button>
-        </Link>
-      }
-    </div>
-  )
+        <br />
+
+        {!!this.props.meal ?
+          <Link to={{
+            pathname: "/foods/new",
+            state: {
+              mealId: this.props.meal.id
+            }
+          }}>
+            <button type="button">
+              Add New Food
+            </button>
+          </Link>
+          :
+          <Link to="/foods/new">
+            <button type="button">
+              Add New Food
+            </button>
+          </Link>
+        }
+
+        {this.renderFoodCards()}
+
+      </div>
+    )
+  }
 }
 
 export default Foods;
