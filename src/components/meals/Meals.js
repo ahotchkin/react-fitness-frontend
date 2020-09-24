b730adaa1b0d47da6eb1a1c95efc77a17c0ed5fc
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-24 14:10:16 -0400

    Filter through meals to find correct meals do display for each diary based on diaryId

--- src/components/meals/Meals.js
+++ src/components/meals/Meals.js
@@ -2,8 +2,11 @@ import React from 'react';
 import MealCard from './MealCard.js';
 
 const Meals = props => {
-  // const mealCards = props.meals.map(meal => <MealCard meal={meal} key={meal.id} mealFoods={meal.attributes.meal_foods} foods={meal.attributes.foods} />)
-  const mealCards = props.meals.map(meal => <MealCard meal={meal} key={meal.id} foods={meal.attributes.foods} />)
+  // const mealCards = props.meals.map(meal => <MealCard meal={meal} key={meal.id} foods={meal.attributes.foods} />)
+
+  // filter desired meals before mapping to avoid a situation where nothing would be returned at the end of map - necessary to avoid warning: Expected to return a value at the end of arrow function array-callback-return
+  const mealCards = props.meals.filter(meal => props.diaryId === meal.relationships.diary.data.id).map(filteredMeal => <MealCard meal={filteredMeal} key={filteredMeal.id} foods={filteredMeal.attributes.foods} />)
+
 
   // in return, going more than one level deep, so need to make sure exerciseCards are there before rendering anything. instead of just saying exerciseCards, need to say: { exerciseCards.length > 0 ? exerciseCards : null }
   // <Link to="/exercises/new">
