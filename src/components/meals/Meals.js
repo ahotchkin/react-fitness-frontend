e1e8d0bbbd115b3a5537aae981956ecb0710d50c
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-28 20:55:40 -0400

    Pass diaryId and diaryDate as props to MealCard

--- src/components/meals/Meals.js
+++ src/components/meals/Meals.js
@@ -5,7 +5,7 @@ const Meals = props => {
   // const mealCards = props.meals.map(meal => <MealCard meal={meal} key={meal.id} foods={meal.attributes.foods} />)
 
   // filter desired meals before mapping to avoid a situation where nothing would be returned at the end of map - necessary to avoid warning: Expected to return a value at the end of arrow function array-callback-return
-  const mealCards = props.meals.filter(meal => props.diaryId === meal.relationships.diary.data.id).map(filteredMeal => <MealCard meal={filteredMeal} key={filteredMeal.id} foods={filteredMeal.attributes.foods} />)
+  const mealCards = props.meals.filter(meal => props.diaryId === meal.relationships.diary.data.id).map(filteredMeal => <MealCard meal={filteredMeal} key={filteredMeal.id} diaryId={props.diaryId} diaryDate={props.diaryDate} foods={filteredMeal.attributes.foods} />)
 
 
   // in return, going more than one level deep, so need to make sure exerciseCards are there before rendering anything. instead of just saying exerciseCards, need to say: { exerciseCards.length > 0 ? exerciseCards : null }
