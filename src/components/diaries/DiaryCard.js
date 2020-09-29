aa6bbed72935326621141d5bab8316bedb32abb2
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-29 17:05:31 -0400

    Add comment reminder

--- src/components/diaries/DiaryCard.js
+++ src/components/diaries/DiaryCard.js
@@ -8,6 +8,7 @@ const DiaryCard = ({ diary }) => {
     <div>
       <h3>Meal Diary for {diary.attributes.date}</h3>
       {console.log("diary: ", diary)}
+      {/* DOES DIARYID NEED TO BE PASSED DOWN THROUGH ALL OF THESE COMPONENTS? AM I EVER USING IT? */}
       <MealsContainer diaryId={diary.id} diaryDate={diary.attributes.date} />
     </div>
 
