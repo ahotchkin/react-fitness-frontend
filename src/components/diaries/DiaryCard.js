27d45eef6e9859252850f3e7859599209f4be537
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-05 15:09:17 -0400

    Display Calories Remaining, Daily Calorie Goal, and Calories Burned for the current day

--- src/components/diaries/DiaryCard.js
+++ src/components/diaries/DiaryCard.js
@@ -3,15 +3,14 @@ import React from 'react';
 // CAN'T GET THE RELATIVE PATH TO WORK....
 import MealsContainer from '/Users/allysonhotchkin/Development/code/react-fitness-app/react-fitness-frontend/src/containers/MealsContainer';
 
-const DiaryCard = ({ diary, caloriesBurned }) => {
+const DiaryCard = props => {
   return (
     <div>
-      <h3>Meal Diary for {diary.attributes.date}</h3>
-      <h4>Remaining Calories = Daily Calories - Consumed Calories + Burned Calories</h4>
-      <h4>XXX (Remaining) = XXX (Daily) - XXX (Consumed) + {caloriesBurned} (Burned)</h4>
-      {console.log("diary: ", diary)}
+      <h3>Meal Diary for {props.diary.attributes.date}</h3>
+      <h4>Calories: {props.currentUser.attributes.daily_calorie_goal + props.caloriesBurned} (Remaining) = {props.currentUser.attributes.daily_calorie_goal} (Daily) - XXX (Consumed) + {props.caloriesBurned} (Burned)</h4>
+      {console.log("diary: ", props.diary)}
       {/* DOES DIARYID NEED TO BE PASSED DOWN THROUGH ALL OF THESE COMPONENTS? AM I EVER USING IT? */}
-      <MealsContainer diaryId={diary.id} diaryDate={diary.attributes.date} />
+      <MealsContainer diaryId={props.diary.id} diaryDate={props.diary.attributes.date} />
     </div>
 
   )
