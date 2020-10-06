2264af897a280d305c71c9cd1aed117b81e124d8
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-05 20:48:14 -0400

    Subtract caloriesConsumed from daily calories

--- src/components/diaries/DiaryCard.js
+++ src/components/diaries/DiaryCard.js
@@ -4,10 +4,11 @@ import React from 'react';
 import MealsContainer from '/Users/allysonhotchkin/Development/code/react-fitness-app/react-fitness-frontend/src/containers/MealsContainer';
 
 const DiaryCard = props => {
+  console.log(props)
   return (
     <div>
       <h3>Meal Diary for {props.diary.attributes.date}</h3>
-      <h4>Calories: {props.currentUser.attributes.daily_calorie_goal + props.caloriesBurned} (Remaining) = {props.currentUser.attributes.daily_calorie_goal} (Daily) - XXX (Consumed) + {props.caloriesBurned} (Burned)</h4>
+      <h4>Calories: {props.currentUser.attributes.daily_calorie_goal} (Daily) - {props.caloriesConsumed} (Consumed) + {props.caloriesBurned} (Burned) = {props.currentUser.attributes.daily_calorie_goal - props.caloriesConsumed + props.caloriesBurned} (Remaining)</h4>
       {console.log("diary: ", props.diary)}
       {/* DOES DIARYID NEED TO BE PASSED DOWN THROUGH ALL OF THESE COMPONENTS? AM I EVER USING IT? */}
       <MealsContainer diaryId={props.diary.id} diaryDate={props.diary.attributes.date} />
