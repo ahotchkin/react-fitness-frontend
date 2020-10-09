79044f4db2866225e8fc19e238e578008e36875a
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-09 18:51:51 -0400

    Display totals for all nutrients and macroNutrients on user dashboa rd

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -68,32 +68,58 @@ const DashboardContainer = props => {
       <p>*************************************************************************************************</p>
 
       <h3>Today's Macros</h3>
-
+      {!!props.dailyMacros ?
+        <div>
+          <ul>
+            Carbohydrates ({props.dailyNutrition.total_carbohydrate}g):
+            <li>Total: {props.dailyMacros.carbohydrates}%</li>
+            <li>Goal: 50%</li>
+          </ul>
+          <ul>
+            Fat ({props.dailyNutrition.total_fat}g)
+            <li>Total: {props.dailyMacros.fat}%</li>
+            <li>Goal: 30%</li>
+          </ul>
+          <ul>
+            Protein ({props.dailyNutrition.protein}g)
+            <li>Total: {props.dailyMacros.protein}%</li>
+            <li>Goal: 20%</li>
+          </ul>
+        </div>
+      :
+        null
+      }
 
       <p>*************************************************************************************************</p>
 
-      <h3>Today's Nutrition</h3>
-        <ul>
-          <li>Total fat: </li>
-          <li>Saturated fat: </li>
-          <li>Polyunsaturated fat: </li>
-          <li>Monounsaturated fat: </li>
-          <li>Trans fat: </li>
-          <li>Cholesterol: </li>
-          <li>Sodium: </li>
-          <li>Total carbohydrate: </li>
-          <li>Dietary fiber: </li>
-          <li>Total sugars: </li>
-          <li>Added sugars: </li>
-          <li>Sugar alcohols: </li>
-          <li>Protein: </li>
-          <li>Vitamin A: </li>
-          <li>Vitamin C: </li>
-          <li>Vitamin D: </li>
-          <li>Calcium: </li>
-          <li>Iron: </li>
-          <li>Potassium: </li>
-        </ul>
+        <h3>Today's Nutrition</h3>
+        {!!props.dailyNutrition ?
+          <div>
+            <ul>
+              <li>Total fat: {props.dailyNutrition.total_fat}g</li>
+              <li>Saturated fat: {props.dailyNutrition.saturated_fat}g</li>
+              <li>Polyunsaturated fat: {props.dailyNutrition.polyunsaturated_fat}g</li>
+              <li>Monounsaturated fat: {props.dailyNutrition.monounsaturated_fat}g</li>
+              <li>Trans fat: {props.dailyNutrition.trans_fat}g</li>
+              <li>Cholesterol: {props.dailyNutrition.cholesterol}mg</li>
+              <li>Sodium: {props.dailyNutrition.sodium}mg</li>
+              <li>Total carbohydrates: {props.dailyNutrition.total_carbohydrate}g</li>
+              <li>Dietary fiber: {props.dailyNutrition.dietary_fiber}g</li>
+              <li>Total sugars: {props.dailyNutrition.total_sugars}g</li>
+              <li>Added sugars: {props.dailyNutrition.added_sugars}g</li>
+              <li>Sugar alcohols: {props.dailyNutrition.sugar_alcohols}g</li>
+              <li>Protein: {props.dailyNutrition.protein}g</li>
+              <li>Vitamin A: {props.dailyNutrition.vitamin_a}%</li>
+              <li>Vitamin C: {props.dailyNutrition.vitamin_c}%</li>
+              <li>Vitamin D: {props.dailyNutrition.vitamin_d}%</li>
+              <li>Calcium: {props.dailyNutrition.calcium}%</li>
+              <li>Iron: {props.dailyNutrition.iron}%</li>
+              <li>Potassium: {props.dailyNutrition.potassium}mg</li>
+            </ul>
+        </div>
+      :
+        null
+      }
 
       <p>*************************************************************************************************</p>
 
