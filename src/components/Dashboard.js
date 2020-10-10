9e5a18cf8285e0817c97dd8c8387e69d591677a9
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-10 18:05:38 -0400

    Disconnect from Redux store, remove mapStateToProps, remove unnecessary comments and console.log, rename component to Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,51 +1,9 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
-// import { getExercises } from '../actions/exercises'
-
-const DashboardContainer = props => {
-
-  // componentDidMount() {
-  //   this.props.getExercises()
-  // }
-
-  // Used in several places, refactor somehow?
-  // const getDate = () => {
-  //   const tzoffset = new Date().getTimezoneOffset() * 60000; //offset in milliseconds
-  //   const date = (new Date(new Date() - tzoffset)).toISOString().split("T")[0];
-  //   return date
-  // }
-  //
-  // // Only updates on page refresh...need to getExercises rather than pull from this.props.currentUser.attributes.exercises
-  // const getTodaysDate = () => {
-  //   const tzoffset = new Date().getTimezoneOffset() * 60000; //offset in milliseconds
-  //   const date = (new Date(new Date() - tzoffset)).toISOString().split("T")[0];
-  //   return date
-  // }
-  //
-  //
-  // const caloriesBurned = () => {
-  //   let data = {}
-  //
-  //   if (!!props.exercises) {
-  //     const todaysExercises = props.exercises.filter(exercise => exercise.attributes.date === getTodaysDate())
-  //     console.log(todaysExercises)
-  //     if (todaysExercises.length === 1) {
-  //       data = {calories_burned: todaysExercises[0].attributes.calories_burned}
-  //     } else if (todaysExercises.length > 1) {
-  //       data = todaysExercises.reduce((a, b) => ({calories_burned: a.attributes.calories_burned + b.attributes.calories_burned}))
-  //     }
-  //   } else {
-  //     data = {calories_burned: 0}
-  //   }
-  //
-  //   return data.calories_burned
-  // }
-
+const Dashboard = props => {
 
   return (
     <div className="DashboardContainer">
-      {console.log(props)}
       <h2>Hello, {props.currentUser.attributes.username}! Welcome to your React Fitness Dashboard!</h2>
 
       <p>*************************************************************************************************</p>
@@ -146,16 +104,4 @@ const DashboardContainer = props => {
 
 }
 
-
-const mapStateToProps = state => {
-  return {
-    currentUser: state.currentUser,
-    // exercises: state.exercises
-  }
-}
-
-// const mapDispatchToProps = {
-//   getExercises
-// }
-//
-export default connect(mapStateToProps)(DashboardContainer);
+export default Dashboard;
