9f50650899af1738f6837b38b5b6c0bd02dbc457
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-12 17:37:09 -0400

    Comment out import of index.css and import bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 // Do I need serviceWorker??????
 import * as serviceWorker from './serviceWorker';
