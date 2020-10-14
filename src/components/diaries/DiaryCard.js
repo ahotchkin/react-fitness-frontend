98d877fb90edb1fe27a2925280204428b45d4f8e
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-14 18:48:41 -0400

    Display remaining calories in color based on number, show formula in table form to generate remaining calories

--- src/components/diaries/DiaryCard.js
+++ src/components/diaries/DiaryCard.js
@@ -3,13 +3,54 @@ import React from 'react';
 import MealsContainer from '../../containers/MealsContainer';
 
 const DiaryCard = props => {
+
+  const caloriesRemaining = () => {
+    // conditional statement is necessary for all props to load before calculating calories remaining
+    if (!!props.currentUser.attributes.daily_calorie_goal && !!props.caloriesConsumed && !!props.caloriesBurned) {
+      return (
+        props.currentUser.attributes.daily_calorie_goal - props.caloriesConsumed + props.caloriesBurned
+      )
+    } else {
+      return null
+    }
+  }
+
   return (
     <div className="col-lg info-container">
       <h3>Meal Diary for {props.diary.attributes.date}</h3>
-      <h4>Calories: {props.currentUser.attributes.daily_calorie_goal} (Daily) - {props.caloriesConsumed} (Consumed) + {props.caloriesBurned} (Burned) = {props.currentUser.attributes.daily_calorie_goal - props.caloriesConsumed + props.caloriesBurned} (Remaining)</h4>
+      <p>Calories Remaining</p>
+      { caloriesRemaining() >= 0 ?
+        <h3 className="positive">{caloriesRemaining()}</h3>
+      :
+        <h3 className="negative">{caloriesRemaining()}</h3>
+      }
+      <table className="calorie-table">
+        <thead>
+          <tr>
+            <td className="calorie-table-cell">{props.currentUser.attributes.daily_calorie_goal}</td>
+            <td className="calorie-table-cell">-</td>
+            <td className="calorie-table-cell">{props.caloriesConsumed}</td>
+            <td className="calorie-table-cell">+</td>
+            <td className="calorie-table-cell">{props.caloriesBurned}</td>
+            <td className="calorie-table-cell">=</td>
+            <td className="calorie-table-cell">{caloriesRemaining()}</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="calorie-table-cell small text-secondary">Goal</td>
+            <td className="calorie-table-cell"></td>
+            <td className="calorie-table-cell small text-secondary">Consumed</td>
+            <td className="calorie-table-cell"></td>
+            <td className="calorie-table-cell small text-secondary">Burned</td>
+            <td className="calorie-table-cell"></td>
+            <td className="calorie-table-cell small text-secondary">Remaining</td>
+          </tr>
+        </tbody>
+      </table>
 
       <hr className="solid-thick" />
-      
+
       <MealsContainer diaryId={props.diary.id} diaryDate={props.diary.attributes.date} />
     </div>
 
