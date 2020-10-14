41fab12c18ec90728b42222f73d579bd00b4de67
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-14 17:25:21 -0400

    Add classes to divs and render exerciseCards in table

--- src/components/exercises/Exercises.js
+++ src/components/exercises/Exercises.js
@@ -8,18 +8,38 @@ const Exercises = props => {
 
   // in return, going more than one level deep, so need to make sure exerciseCards are there before rendering anything. instead of just saying exerciseCards, need to say: { exerciseCards.length > 0 ? exerciseCards : null }
   return (
-    <div>
-      <h2>Exercises for {props.date}</h2>
-      <h3>Total calories burned on {props.date}: {props.caloriesBurned}</h3>
-      
-      { exerciseCards.length > 0 ? exerciseCards : null }
+    <div className="row">
+      <div className="col-lg info-container">
+        <h2>Exercises for {props.date}</h2>
+        <h3>Total calories burned on {props.date}: {props.caloriesBurned}</h3>
 
-      <Link to="/exercises/new">
-        <button type="button">
-          Add Exercise
-        </button>
-      </Link>
+        <Link to="/exercises/new">
+          <button className="btn btn-primary-outline btn-sm" type="button">
+            Add Exercise
+          </button>
+        </Link>
 
+        <hr className="solid-thick" />
+
+        { exerciseCards.length > 0 ?
+          <table className="table table-borderless table-sm" id="exercises">
+            <thead>
+              <tr>
+                <th>Exercise</th>
+                <th className="right-align">Minutes</th>
+                <th className="right-align">Calories</th>
+                <th className="right-align"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {exerciseCards}
+            </tbody>
+          </table>
+        :
+          null
+        }
+
+      </div>
     </div>
   )
 }
