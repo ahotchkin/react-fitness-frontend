7b2470a55e1994e7d9f6e675074cf9ed21d78265
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-17 20:43:15 -0400

    Add styles and remove submit button as it is not needed

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -3,17 +3,21 @@ import React from 'react';
 const SearchInput = props => {
 
   return(
+
     <form onSubmit={props.handleSubmit}>
       <input
         type="text"
+        className="form-control"
         name="searchTerm"
+        id="searchTerm"
         onChange={props.handleOnChange}
+        placeholder="Search the database"
         value={props.searchTerm}
       />
-
+      {/*
       <br />
-
       <input type="submit" value="Search" />
+      */}
     </form>
   )
 
