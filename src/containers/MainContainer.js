374c38c0f6cb7185e5031a795f5bbe691a43b8ec
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-21 18:13:59 -0400

    Rewrite mealNutrition function to get all nutrition info, not just calories; write mealMacros function to determine macros for each meal of the day and pass as props for each meal to Dashboard

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -17,7 +17,8 @@ import ExercisesContainer from './ExercisesContainer';
 import FoodsContainer from './FoodsContainer';
 import MealFoodsContainer from './MealFoodsContainer';
 
-import NavBar from '../components/NavBar'
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
 
 // Add Switch and wrap routes?
 
@@ -92,7 +93,30 @@ class MainContainer extends Component {
 
 
   mealNutrition = selectedMeal => {
-    let meal = {}
+    let mealMealFoods = []
+
+    let mealTotal = {
+      added_sugars: 0,
+      calcium: 0,
+      calories: 0,
+      cholesterol: 0,
+      dietary_fiber: 0,
+      iron: 0,
+      monounsaturated_fat: 0,
+      polyunsaturated_fat: 0,
+      potassium: 0,
+      protein: 0,
+      saturated_fat: 0,
+      sodium: 0,
+      sugar_alcohols: 0,
+      total_carbohydrate: 0,
+      total_fat: 0,
+      total_sugars: 0,
+      trans_fat: 0,
+      vitamin_a: 0,
+      vitamin_c: 0,
+      vitamin_d: 0
+    }
 
     const todaysDiary = this.props.diaries.find(diary => diary.attributes.date === this.getDate(new Date()))
     // Need to get meals from Redux Store rather than from diary.attributes or else /diaries will not refresh if mealFood is deleted
@@ -100,12 +124,61 @@ class MainContainer extends Component {
       const todaysMeals = this.props.meals.filter(meal => meal.relationships.diary.data.id === todaysDiary.id).map(filteredMeal => filteredMeal.attributes)
 
       if (todaysMeals.length > 0) {
-        meal = todaysMeals.find(meal => meal.category.toLowerCase() === selectedMeal)
+        mealMealFoods = todaysMeals.find(meal => meal.category.toLowerCase() === selectedMeal).meal_foods
       }
-    } else {
-      meal = {calories: 0}
     }
-    return meal
+
+    if (mealMealFoods.flat().length > 0) {
+      mealTotal = mealMealFoods.flat().reduce((a, b) => {
+        for (let k in b) {
+          if (b.hasOwnProperty(k) && k !== "id" && k !== "meal_id" && k !== "food_id" && k !== "number_of_servings" && k !== "created_at" && k !== "updated_at")
+            a[k] = (a[k] || 0) + b[k];
+        }
+        return a;
+        // by adding " , {}" to the end, it returns the new object with the 3 properties above removed. What is happening here?
+      }, {});
+    }
+
+    mealTotal.meal = selectedMeal
+    return mealTotal
+  }
+
+  mealMacros = selectedMeal => {
+    let macros = {"meal": selectedMeal, "carbohydrates": 0, "fat": 0, "protein": 0}
+
+    if (!!this.mealNutrition(selectedMeal)) {
+      const mealNutrition = this.mealNutrition(selectedMeal)
+
+      const totalMealNutrition = mealNutrition.total_carbohydrate + mealNutrition.total_fat + mealNutrition.protein
+
+      if (totalMealNutrition !== 0) {
+        macros.carbohydrates = Math.round((mealNutrition.total_carbohydrate / totalMealNutrition) * 100)
+        macros.fat = Math.round((mealNutrition.total_fat / totalMealNutrition) * 100)
+        macros.protein = Math.round((mealNutrition.protein / totalMealNutrition) * 100)
+      }
+
+      return macros
+    }
+
+
+    // default all users' goals to carbohydrates: 50%, fat: 30%, protein: 20%
+    // eventually add these attributes to user and allow user to set individual goals
+
+    // To Calculate Macros by grams per day:
+    // Carbs:
+    // 4 calories per gram
+    // 40% of 2,000 calories = 800 calories of carbs per day
+    // Total grams of carbs allowed per day = 800/4 = 200 grams
+    //
+    // Fats:
+    // 9 calories per gram
+    // 30% of 2,000 calories = 600 calories of protein per day
+    // Total grams of fat allowed per day = 600/9 = 67 grams
+    //
+    // Proteins:
+    // 4 calories per gram
+    // 30% of 2,000 calories = 600 calories of protein per day
+    // Total grams of protein allowed per day = 600/4 = 150 grams  }
   }
 
   dailyNutrition = () => {
@@ -208,10 +281,8 @@ class MainContainer extends Component {
     // Total grams of protein allowed per day = 600/4 = 150 grams
   }
 
-
-
-
   render() {
+    console.log(this.mealNutrition("breakfast"))
     return (
       // Update className when adding css
       <div>
@@ -220,7 +291,7 @@ class MainContainer extends Component {
         {/* is there a way to always redirect to "/" if not logged in? except for /login and /signup */}
 
         <Switch>
-          <Route exact path="/" render={ () => this.props.loggedIn ? <Dashboard currentUser={this.props.currentUser} caloriesBurned={this.caloriesBurned()} breakfastNutrition={this.mealNutrition("breakfast")} lunchNutrition={this.mealNutrition("lunch")} dinnerNutrition={this.mealNutrition("dinner")} snacksNutrition={this.mealNutrition("snacks")} dailyMacros={this.dailyMacros()} dailyNutrition={this.dailyNutrition()} /> : <Home /> }  />
+          <Route exact path="/" render={ () => this.props.loggedIn ? <Dashboard currentUser={this.props.currentUser} caloriesBurned={this.caloriesBurned()} breakfastNutrition={this.mealNutrition("breakfast")} breakfastMacros={this.mealMacros("breakfast")} lunchNutrition={this.mealNutrition("lunch")} lunchMacros={this.mealMacros("lunch")} dinnerNutrition={this.mealNutrition("dinner")} dinnerMacros={this.mealMacros("dinner")} snacksNutrition={this.mealNutrition("snacks")} snacksMacros={this.mealMacros("snacks")} dailyNutrition={this.dailyNutrition()} dailyMacros={this.dailyMacros()} /> : <Home /> }  />
 
           {/* below routes should only be available to users who are NOT logged in */}
           <Route exact path="/login" render={ props => this.props.loggedIn ? <Redirect to="/" /> : <Login history={props.history}/> } />
@@ -263,6 +334,8 @@ class MainContainer extends Component {
 
           <Route />
         </Switch>
+
+        <Footer />
       </div>
 
     );
