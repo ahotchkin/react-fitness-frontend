89225f9a64a4d9a39c39c468074ce5a115973653
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-21 18:10:45 -0400

    Style home page and add icons for possible use

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,16 +3,23 @@ import React from 'react'
 // import SignUp from './SignUp'
 import { Link } from 'react-router-dom'
 
+import HealthyFood from './icons/HealthyFood';
+import Barbell from './icons/Barbell';
+
 const Home = ({}) => (
-  <div>
-    <h2>Welcome to React Fitness!</h2>
+  <div className="welcome-container center-align">
+    <h2>React Fitness</h2>
+    <br />
+    <span><HealthyFood />   </span>
+    <span><Barbell />   </span>
+    <br />
     <Link to="/signup">
-      <button type="button">
+      <button className="btn btn-primary-fill btn-home" type="button">
         Sign Up
       </button>
     </Link>
     <Link to="/login">
-      <button type="button">
+      <button className="btn btn-primary-fill btn-home" type="button">
         Log In
       </button>
     </Link>
