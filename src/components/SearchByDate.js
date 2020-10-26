b85f5b36b1152aa99de666bb2ef8f1b3194b5303
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-26 17:32:28 -0400

    Add styling to be used across pages

--- src/components/SearchByDate.js
+++ src/components/SearchByDate.js
@@ -5,11 +5,22 @@ import "react-datepicker/dist/react-datepicker.css";
 const SearchByDate = props => {
 
   return (
-    <DatePicker
-      todayButton="Today"
-      selected={props.startDate}
-      onChange={props.handleOnChange}
-    />
+    <div className="datepicker-container">
+      <div className="col-lg">
+        <div className="row">
+            <div className="col-auto">
+              <h3>{props.header}</h3>
+            </div>
+            <div className="col-sm-3">
+              <DatePicker
+              todayButton="Today"
+              selected={props.startDate}
+              onChange={props.handleOnChange}
+              />
+            </div>
+          </div>
+        </div>
+    </div>
   );
 
 }
