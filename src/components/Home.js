e8991e96cf5169510953a74e8faf4e984e110537
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-26 17:32:06 -0400

    Import and render footer

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,41 +3,48 @@ import React from 'react'
 // import SignUp from './SignUp'
 import { Link } from 'react-router-dom'
 
+import Footer from '../components/Footer';
+
 import fruit from '../icons/fruit.svg';
 import dumbbell from '../icons/dumbbell.svg';
 import weightScale from '../icons/weightScale.svg';
 import foodDiary from '../icons/foodDiary.svg';
 
 const Home = ({}) => (
-  <div className="welcome-container center-align">
-    <h2>React Fitness</h2>
-    {/* FIGURE OUT HOW TO MAKE THIS RESPONSE SO ICONS MOVE CLOSER TOGETHER RATHER THAN STACK */}
-    <div className="container icon-container">
-      <div className="row">
-        <div className="col-sm">
-          <img src={fruit} alt="Fruit" />
-        </div>
-        <div className="col-sm">
-          <img src={foodDiary} alt="Food Diary" />
-        </div>
-        <div className="col-sm">
-          <img src={dumbbell} alt="Dumbbell" />
-        </div>
-        <div className="col-sm">
-          <img src={weightScale} alt="Scale" />
+  <div>
+    <div className="welcome-container center-align">
+      <h2>React Fitness</h2>
+      {/* FIGURE OUT HOW TO MAKE THIS RESPONSE SO ICONS MOVE CLOSER TOGETHER RATHER THAN STACK */}
+      <div className="container icon-container">
+        <div className="row">
+          <div className="col-sm">
+            <img src={fruit} alt="Fruit" />
+          </div>
+          <div className="col-sm">
+            <img src={foodDiary} alt="Food Diary" />
+          </div>
+          <div className="col-sm">
+            <img src={dumbbell} alt="Dumbbell" />
+          </div>
+          <div className="col-sm">
+            <img src={weightScale} alt="Scale" />
+          </div>
         </div>
       </div>
+      <Link to="/signup">
+        <button className="btn btn-primary-fill btn-home" type="button">
+          Sign Up
+        </button>
+      </Link>
+      <Link to="/login">
+        <button className="btn btn-primary-fill btn-home" type="button">
+          Log In
+        </button>
+      </Link>
     </div>
-    <Link to="/signup">
-      <button className="btn btn-primary-fill btn-home" type="button">
-        Sign Up
-      </button>
-    </Link>
-    <Link to="/login">
-      <button className="btn btn-primary-fill btn-home" type="button">
-        Log In
-      </button>
-    </Link>
+    <div className="home-footer">
+      <Footer />
+    </div>
   </div>
 );
 
