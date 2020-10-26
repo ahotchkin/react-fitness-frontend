8ed58ad7ea2eaaf965d266ba4697adf4dfae44ec
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-26 12:51:30 -0400

    Add footer component for icons attribution

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="footer attribution-footer text-muted">
+      Icons made by <a href="https://www.flaticon.local/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.local/" title="Flaticon">www.flaticon.local</a>
+    </div>
+  )
+}
+
+export default Footer;
