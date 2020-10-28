71f9ad53a460e03a3a723bbc009ccf655020ec18
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-28 18:34:28 -0400

    Add submitted to state and set to false, add handleOnClick event to set this.state.submitted to true on form submission, remove setState of all fields to  after form submission, add error classes and content to form

--- src/components/exercises/ExerciseUpdate.js
+++ src/components/exercises/ExerciseUpdate.js
@@ -7,7 +7,8 @@ class ExerciseUpdate extends Component {
     category: this.props.exercise.attributes.category,
     name: this.props.exercise.attributes.name,
     duration_in_minutes: this.props.exercise.attributes.duration_in_minutes,
-    calories_burned: this.props.exercise.attributes.calories_burned
+    calories_burned: this.props.exercise.attributes.calories_burned,
+    submitted: false
   }
 
   handleOnChange = event => {
@@ -18,17 +19,16 @@ class ExerciseUpdate extends Component {
     })
   }
 
+  handleOnClick = event => {
+    this.setState({
+      submitted: true
+    })
+  }
+
   handleSubmit = event => {
     event.preventDefault();
 
     this.props.updateExercise(this.state, this.props.date, this.props.exercise, this.props.history)
-    // this.setState({
-    //   // drop down stays selected on whatever category was selected. Is this a problem or will it always update on page refresh?
-    //   // category: "",
-    //   // name: "",
-    //   // duration_in_minutes: "",
-    //   // calories_burned: ""
-    // });
   };
 
   render() {
@@ -36,19 +36,19 @@ class ExerciseUpdate extends Component {
       <div className="dashboard-container">
         <div className="row">
           <div className="col-lg info-container">
-
             <div className="form">
-            {console.log(this.state.category)}
-
               <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
                   <label htmlFor="category">Category: </label>
-                  <select className="form-control" name="category" id="category" value={this.state.category} onChange={this.handleOnChange}>
+                  <select className={`form-control ${!!this.state.submitted && this.state.category === "" ? "is-invalid" : null}`} name="category" id="category" value={this.state.category} onChange={this.handleOnChange}>
                     <option value="cardio">Cardio</option>
                     <option value="strength">Strength Training</option>
                     <option value="balance">Balance</option>
                     <option value="stretching">Stretching</option>
                   </select>
+                  <div className="invalid-feedback">
+                    Category required
+                  </div>
                 </div>
 
                 <div className="form-group">
@@ -70,28 +70,34 @@ class ExerciseUpdate extends Component {
                     <label htmlFor="duration_in_minutes">Minutes Performed: </label>
                     <input
                       type="number"
-                      className="form-control"
+                      className={`form-control ${!!this.state.submitted && this.state.duration_in_minutes === "" ? "is-invalid" : null}`}
                       name="duration_in_minutes"
                       id="duration_in_minutes"
                       onChange={this.handleOnChange}
                       value={this.state.duration_in_minutes}
                     />
+                    <div className="invalid-feedback">
+                      Minutes Performed required
+                    </div>
                   </div>
 
                   <div className="form-group col-md-6">
                     <label htmlFor="calories_burned">Calories Burned: </label>
                     <input
                       type="number"
-                      className="form-control"
+                      className={`form-control ${!!this.state.submitted && this.state.calories_burned === "" ? "is-invalid" : null}`}
                       name="calories_burned"
                       id="calories_burned"
                       onChange={this.handleOnChange}
                       value={this.state.calories_burned}
                     />
+                    <div className="invalid-feedback">
+                      Calories Burned required
+                    </div>
                   </div>
                 </div>
 
-                <input type="submit" className="btn btn-primary-fill" value="Update Exercise" />
+                <input type="submit" className="btn btn-primary-fill" value="Update Exercise" onClick={this.handleOnClick} />
 
               </form>
             </div>
