4c482e24ff58e7ca7c36c5cdf83f7ce660cf06a1
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-30 17:30:20 -0400

    Remove unnecessary comments

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -15,14 +15,12 @@ class SignUp extends Component {
     height_inches: "",
     weight: "",
     lifestyle: "",
-    submitted: false,
+    submitted: false
   }
 
 
   calculateDailyCalorieGoal = () => {
-    // add conditional so this is never under 1200
     let bmr = 0
-    //
     let activity = 0
 
     if (this.state.lifestyle === "notActive") {
@@ -49,11 +47,8 @@ class SignUp extends Component {
       bmr = 1200
     }
 
-    console.log(parseInt(bmr))
     return parseInt(bmr)
-    // console.log(this.state.lifestyle)
-    // console.log(activity)
-    // return activity
+
     // Mifflin-St Jeor Equation:
     // For men:
     // BMR = 10W + 6.25H - 5A + 5
@@ -73,230 +68,6 @@ class SignUp extends Component {
     // Moderately active: * 1.65
     // Very active: * 1.8
   }
-
-  // calculateDailyFatGoal = () => {
-  //   const dailyCalorieGoal = this.calculateDailyCalorieGoal()
-  //   const fat = parseInt((dailyCalorieGoal * .30) / 9)
-  //   return fat
-  //
-  //   //     {/* Goal is 30% of DV */}
-  //   //     <li>Total fat: {props.dailyNutrition.total_fat}g</li>
-  //
-  //   //   Fats:
-  //   //   9 calories per gram
-  //   //   30% of 2,000 calories = 600 calories of protein per day
-  //   //   Total grams of fat allowed per day = 600/9 = 67 grams
-  //
-  //
-  // }
-  //
-  // calculateDailySaturatedFatGoal = () => {
-  //   const dailyCalorieGoal = this.calculateDailyCalorieGoal()
-  //   const saturatedFat = parseInt((dailyCalorieGoal * .07) / 9)
-  //   return saturatedFat
-  //   //     {/* Goal is <7% of DV */}
-  //   //     <li>Saturated fat: {props.dailyNutrition.saturated_fat}g</li>
-  //
-  //   //   Fats:
-  //   //   9 calories per gram
-  //   //   30% of 2,000 calories = 600 calories of protein per day
-  //   //   Total grams of fat allowed per day = 600/9 = 67 grams
-  //
-  // }
-  //
-  // calculateDailyPolyunsaturatedFatGoal = () => {
-  //   const dailyCalorieGoal = this.calculateDailyCalorieGoal()
-  //   const polyunsaturatedFat = parseInt((dailyCalorieGoal * .10) / 9)
-  //   return polyunsaturatedFat
-  //   //     {/* Goal is up to 10% of DV */}
-  //   //     <li>Polyunsaturated fat: {props.dailyNutrition.polyunsaturated_fat}g</li>
-  //
-  //   //   Fats:
-  //   //   9 calories per gram
-  //   //   30% of 2,000 calories = 600 calories of protein per day
-  //   //   Total grams of fat allowed per day = 600/9 = 67 grams
-  //
-  // }
-  //
-  // calculateDailyMonounsaturatedFatGoal = () => {
-  //   const dailyCalorieGoal = this.calculateDailyCalorieGoal()
-  //   const monounsaturatedFat = parseInt((dailyCalorieGoal * .20) / 9)
-  //   return monounsaturatedFat
-  //   //     {/* Goal is up to 20% of DV */}
-  //   //     <li>Monounsaturated fat: {props.dailyNutrition.monounsaturated_fat}g</li>
-  //
-  //   //   Fats:
-  //   //   9 calories per gram
-  //   //   30% of 2,000 calories = 600 calories of protein per day
-  //   //   Total grams of fat allowed per day = 600/9 = 67 grams
-  //
-  // }
-  //
-  // calculateDailyTransFatGoal = () => {
-  //   const transFat = 0
-  //   return transFat
-  //   //     {/* Goal is 0% of DV */}
-  //   //     <li>Trans fat: {props.dailyNutrition.trans_fat}g</li>
-  //
-  // }
-  //
-  // calculateDailyCholesterolGoal = () => {
-  //   const cholesterol = 300
-  //   return cholesterol
-  //
-  //   //     {/* Goal is <300mg */}
-  //   //     <li>Cholesterol: {props.dailyNutrition.cholesterol}mg</li>
-  //
-  // }
-  //
-  // calculateDailySodiumGoal = () => {
-  //   const sodium = 2300
-  //   return sodium
-  //
-  //   //     {/* Goal is 2,300mg */}
-  //   //     <li>Sodium: {props.dailyNutrition.sodium}mg</li>
-  //
-  // }
-  //
-  // calculateDailyCarbohydrateGoal = () => {
-  //   const dailyCalorieGoal = this.calculateDailyCalorieGoal()
-  //   const carbohydrate = parseInt((dailyCalorieGoal * .50) / 4)
-  //   return carbohydrate
-  //
-  //   //     {/* Goal is 50% of DV */}
-  //   //     <li>Total carbohydrates: {props.dailyNutrition.total_carbohydrate}g</li>
-  //
-  //   //   Carbs:
-  //   //   4 calories per gram
-  //   //   40% of 2,000 calories = 800 calories of carbs per day
-  //   //   Total grams of carbs allowed per day = 800/4 = 200 grams
-  //
-  // }
-  //
-  // calculateDailyFiberGoal = () => {
-  //   const fiber = 25
-  //   return fiber
-  //
-  //   //     {/* Goal is 25g a day */}
-  //   //     <li>Dietary fiber: {props.dailyNutrition.dietary_fiber}g</li>
-  //
-  // }
-  //
-  // calculateDailySugarGoal = () => {
-  //   let sugar = 0
-  //   if (this.state.gender === "male") {
-  //     sugar = 38
-  //   } else if (this.state.gender === "female") {
-  //     sugar = 25
-  //   }
-  //
-  //   return sugar
-  //
-  //   //     {/* Goal is MALE: 38g, FEMALE: 25g */}
-  //   //     <li>Total sugars: {props.dailyNutrition.total_sugars}g</li>
-  //
-  // }
-  //
-  // calculateDailyProteinGoal = () => {
-  //   const dailyCalorieGoal = this.calculateDailyCalorieGoal()
-  //   const protein = parseInt((dailyCalorieGoal * .20) / 4)
-  //   return protein
-  //
-  //   //     {/* Goal is 20% of DV */}
-  //   //     <li>Protein: {props.dailyNutrition.protein}g</li>
-  //
-  //   //   Proteins:
-  //   //   4 calories per gram
-  //   //   30% of 2,000 calories = 600 calories of protein per day
-  //   //   Total grams of protein allowed per day = 600/4 = 150 grams
-  //
-  // }
-  //
-  // calculateDailyVitaminAGoal = () => {
-  //   let vitaminA = 0
-  //   if (this.state.gender === "male") {
-  //     vitaminA = 900
-  //   } else if (this.state.gender === "female") {
-  //     vitaminA = 700
-  //   }
-  //
-  //   return vitaminA
-  //
-  //   //     {/* Goal is MALE: 900 mcg RAE, FEMALE: 700 mcg RAE */}
-  //   //     <li>Vitamin A: {props.dailyNutrition.vitamin_a}%</li>
-  //
-  // }
-  //
-  // calculateDailyVitaminCGoal = () => {
-  //   let vitaminC = 0
-  //   if (this.state.gender === "male") {
-  //     vitaminC = 90
-  //   } else if (this.state.gender === "female") {
-  //     vitaminC = 75
-  //   }
-  //
-  //   return vitaminC
-  //
-  //   //     {/* Goal is MALE: 90mg, FEMALE: 75mg */}
-  //   //     <li>Vitamin C: {props.dailyNutrition.vitamin_c}%</li>
-  //
-  // }
-  //
-  // calculateDailyVitaminDGoal = () => {
-  //   let vitaminD = 0
-  //   if (parseInt(this.state.age) <= 70) {
-  //     vitaminD = 15
-  //   } else if (parseInt(this.state.age) >= 71) {
-  //     vitaminD = 20
-  //   }
-  //
-  //   return vitaminD
-  //
-  //   // {/* Goal is AGE 19-70: 15 mcg (600 IU), AGE 71+: 20 mcg (800 IU) */}
-  //   // <li>Vitamin D: {props.dailyNutrition.vitamin_d}%</li>
-  //
-  // }
-  //
-  //
-  //
-  // calculateDailyCalciumGoal = () => {
-  //   let calcium = 0
-  //   if ((parseInt(this.state.age) <= 50) || (parseInt(this.state.age) >= 51 && parseInt(this.state.age) <= 70 && this.state.gender === "male")) {
-  //     calcium = 1000
-  //   } else if ((parseInt(this.state.age)) >= 71 || (parseInt(this.state.age) >= 51 && parseInt(this.state.age) <= 70 && this.state.gender === "female")) {
-  //     calcium = 1200
-  //   }
-  //   return calcium
-  //
-  //   // {/* Goal is AGE 19-50: 1,000 mg, MALE AGE 51-70: 1,000 mg, FEMALE AGE 51-70: 1,200 mg, AGE 71+: 1,200 mg */}
-  //   // <li>Calcium: {props.dailyNutrition.calcium}%</li>
-  //
-  // }
-  //
-  // calculateDailyIronGoal = () => {
-  //   let iron = 0
-  //   if ((parseInt(this.state.age) <= 50 && this.state.gender === "male") || (parseInt(this.state.age) >= 51)) {
-  //     iron = 8
-  //   } else if (parseInt(this.state.age) <= 50 && this.state.gender === "female") {
-  //     iron = 18
-  //   }
-  //
-  //   return iron
-  //
-  //   // {/* Goal is MALE AGE 19-50: 8mg, FEMALE AGE 19-50: 18mg, AGE 51+: 8mg */}
-  //   // <li>Iron: {props.dailyNutrition.iron}%</li>
-  //
-  // }
-  //
-  // calculateDailyPotassiumGoal = () => {
-  //   const potassium = 4700
-  //   return potassium
-  //
-  //   // {/* Goal is 4700mg */}
-  //   // <li>Potassium: {props.dailyNutrition.potassium}mg</li>
-  //
-  // }
-
 
   calculateDailyNutrientGoals = () => {
     const dailyCalorieGoal = this.calculateDailyCalorieGoal()
@@ -352,10 +123,9 @@ class SignUp extends Component {
     // what does this do????
     event.persist()
 
-    console.log(this.state)
     this.setState({
       [event.target.name]: event.target.value
-    }, console.log(`${[event.target.name]}: ${event.target.value}`))
+    })
   }
 
   handleOnClick = event => {
@@ -368,23 +138,7 @@ class SignUp extends Component {
     event.preventDefault()
     const dailyCalorieGoal = this.calculateDailyCalorieGoal()
     const dailyNutrientGoals = this.calculateDailyNutrientGoals()
-    {console.log(dailyCalorieGoal)}
-    console.log(dailyNutrientGoals)
-
-    // this.state.height_inches === "" ? this.state.height_inches = 0 : null
-
     this.props.signUp(this.state, dailyCalorieGoal, dailyNutrientGoals, this.props.history)
-    // this.setState({
-    //   username: "",
-    //   password: "",
-    //   gender: "",
-    //   age: "",
-    //   height_feet: "",
-    //   height_inches: "",
-    //   weight: "",
-    //   lifestyle: "",
-    // })
-
   }
 
   render() {
