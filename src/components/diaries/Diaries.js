ca70174149d75de2aaefbd6d31995170bb26ff75
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-30 13:56:17 -0400

    Add function to change date format to display in Start Diary button

--- src/components/diaries/Diaries.js
+++ src/components/diaries/Diaries.js
@@ -157,12 +157,21 @@ const Diaries = props => {
 
   const createDiaryCards = props.diaries.filter(diary => diary.attributes.date === props.date).map(filteredDiary => <DiaryCard diary={filteredDiary} key={filteredDiary.id} currentUser={props.currentUser} caloriesConsumed={props.caloriesConsumed} caloriesBurned={props.caloriesBurned} />)
 
+  const displayDate = () => {
+    const date = props.date.match(/\d+/g)
+    const year = date[0]
+    const month = date[1]
+    const day = date[2]
+
+    return `${month}/${day}/${year}`
+  }
+
   const renderDiaryCards = () => {
     if (createDiaryCards.length > 0) {
       return createDiaryCards
     } else {
       // move createDiary to separate function?
-      return <button className="btn btn-primary-fill auto-width" onClick={() => props.createDiary(props.date, props.currentUser, props.history)}>Start Meal Diary for {props.date}</button>
+      return <button className="btn btn-primary-fill auto-width" onClick={() => props.createDiary(props.date, props.currentUser, props.history)}>Start Meal Diary for {displayDate()}</button>
     }
   }
 
