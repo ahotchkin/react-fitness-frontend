e7bd7b1b06bea5b0b8a295dcca37ed3020870763
ahotchkin <allyson.hotchkin@gmail.com>
2020-11-01 11:48:06 -0500

    Add semi-colons and format

--- src/store.js
+++ src/store.js
@@ -2,12 +2,12 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 // import { compose } from 'redux' ???????????????
 import thunk from 'redux-thunk';
 
-import currentUser from './reducers/currentUser'
-import exercises from './reducers/exercises'
-import diaries from './reducers/diaries'
-import meals from './reducers/meals'
-import foods from './reducers/foods'
-import mealFoods from './reducers/mealFoods'
+import currentUser from './reducers/currentUser';
+import exercises from './reducers/exercises';
+import diaries from './reducers/diaries';
+import meals from './reducers/meals';
+import foods from './reducers/foods';
+import mealFoods from './reducers/mealFoods';
 
 // could separate this into a separate rootReducer file
 const reducer = combineReducers({
@@ -17,11 +17,11 @@ const reducer = combineReducers({
   meals,
   foods,
   mealFoods
-})
+});
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
 // const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk));
 
-export default store
+export default store;
